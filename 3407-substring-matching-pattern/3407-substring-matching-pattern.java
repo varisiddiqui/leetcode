721c192517f4a46d2ed60cc8272e780e96ac7d84
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-01-05 11:59:34 +0530

    Time: 3 ms (0%), Space: 43.3 MB (50%) - LeetHub

--- 3407-substring-matching-pattern/3407-substring-matching-pattern.java
+++ 3407-substring-matching-pattern/3407-substring-matching-pattern.java
@@ -1,10 +1,13 @@
 class Solution {
     public boolean hasMatch(String s, String p) {
+        String str = p.replace("*", "");
+        if(s.contains(str))
+        return true;
         String start = p.substring(0, p.indexOf('*'));
         String end = p.substring(p.indexOf('*') + 1);
         int i = s.indexOf(start);
-        int j = s.lastIndexOf(end);
-        if( (i+start.length() - 1) < j)
+        int j = s.indexOf(end , i+start.length());
+        if(i!= -1 && j!= -1  && i < j)
         return true;
 
         return false;
