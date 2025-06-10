cb1e396be15830d99de0411ca7104a794e6f8f03
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-06-10 22:16:31 +0530

    Time: 2 ms (69.4%), Space: 41.5 MB (67.42%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,20 +1,15 @@
 class Solution {
     public int hIndex(int[] citations) {
-        int max = 0;
+      
         Arrays.sort(citations);
         int n = citations.length;
 
-        for(int i=0; i<n; i++){
-            int atleast = n-i;
-            if(citations[i] <= atleast){
-                max = Math.max(max, citations[i]);
-            }
-        }
+        
         int count=0;
         for(int i=n-1; i>=0; i--){
             if(citations[i] > count) count++;
             else break;
         }
-        return Math.max(max, count);
+        return count;
     }
 }
