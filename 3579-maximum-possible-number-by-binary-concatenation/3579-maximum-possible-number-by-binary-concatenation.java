1ae69fbd0baf02c1a0f8b3c6a1aae65900eae868
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-06-29 16:21:44 +0530

    Time: 10 ms (29.49%), Space: 43.6 MB (25.64%) - LeetHub

--- 3579-maximum-possible-number-by-binary-concatenation/3579-maximum-possible-number-by-binary-concatenation.java
+++ 3579-maximum-possible-number-by-binary-concatenation/3579-maximum-possible-number-by-binary-concatenation.java
@@ -5,30 +5,23 @@ class Solution {
             bin[i]=Integer.toBinaryString(nums[i]);
         }
         int max=0;
-        //first Combination
-        String bin1 = bin[0]+bin[1]+bin[2];
-        int tot = Integer.parseInt(bin1, 2);
-        max = Math.max(max, tot);
-        //2
-        bin1 = bin[0]+bin[2]+bin[1];
-        tot = Integer.parseInt(bin1, 2);
-        max = Math.max(max, tot);
+        int arr[][] = { {0, 1, 2},
+        {0, 2, 1},
+        {1, 0, 2},
+        {1, 2, 0},
+        {2, 0, 1},
+        {2, 1, 0}};
+
+        for(int i=0; i<6; i++){
+            int idx1 = arr[i][0];
+            int idx2 = arr[i][1];
+            int idx3 = arr[i][2];
+            String concat = bin[idx1]+bin[idx2]
+            +bin[idx3];
+            int total = Integer.parseInt(concat, 2);
+            max = Math.max(max, total);
+        }
         
-        bin1 = bin[2]+bin[0]+bin[1];
-        tot = Integer.parseInt(bin1, 2);
-        max = Math.max(max, tot);
-
-        bin1 = bin[2]+bin[1]+bin[0];
-        tot = Integer.parseInt(bin1, 2);
-        max = Math.max(max, tot);
-
-        bin1 = bin[1]+bin[2]+bin[0];
-        tot = Integer.parseInt(bin1, 2);
-        max = Math.max(max, tot);
-
-        bin1 = bin[1]+bin[0]+bin[2];
-        tot = Integer.parseInt(bin1, 2);
-        max = Math.max(max, tot);
 
         return max;
 
