3f10253caef22daadd39d3f023162cd65361e99f
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-06-30 21:46:40 +0530

    Time: 49 ms (90.23%), Space: 58.3 MB (29.32%) - LeetHub

--- 3510-maximize-the-total-height-of-unique-towers/3510-maximize-the-total-height-of-unique-towers.java
+++ 3510-maximize-the-total-height-of-unique-towers/3510-maximize-the-total-height-of-unique-towers.java
@@ -1,26 +1,34 @@
 class Solution {
     public long maximumTotalSum(int[] maximumHeight) {
-        Arrays.sort(maximumHeight);
-
         int n = maximumHeight.length;
-        System.out.println(n);
-        if(maximumHeight[n-1]<n) return -1;
+        Arrays.sort(maximumHeight);
         long sum=maximumHeight[n-1];
+        if(maximumHeight[n-1] < n) return -1;
 
         for(int i=n-2; i>=0; i--){
-            if(maximumHeight[i]>=(i+1)){
-                if(maximumHeight[i]<maximumHeight[i+1])
-                sum += (long)maximumHeight[i];
-                else{
-                    maximumHeight[i]=maximumHeight[i+1]-1;;
+            if(maximumHeight[i]<(i+1)){
+                return -1;
+            }
+            else{
+                if(maximumHeight[i]>=(i+1)){
+                    if(maximumHeight[i]<maximumHeight[i+1]){
+                        sum += maximumHeight[i];
+                    }
+                    else if(maximumHeight[i]>=maximumHeight[i+1]){
+                        if(maximumHeight[i+1]-1 <=0) return -1;
+                        else {
+                            maximumHeight[i]=maximumHeight[i+1]-1;
+                            sum += maximumHeight[i];
+                        }
+                        
+                    }
                     
-                    sum += (long)maximumHeight[i];
-                     
                 }
+                else 
+                return -1;
+            
                 
             }
-            
-            else return -1;
         }
         return sum;
     }
