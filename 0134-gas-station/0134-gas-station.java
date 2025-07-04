6e9b8adf741ad6e8c0128e7182ada35811bf8bbf
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-07-04 18:33:24 +0530

    Time: 4 ms (11.5%), Space: 56.3 MB (44.31%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,45 +1,30 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int n = gas.length;
-
-        int diff[] = new int[n];
-        diff[0] = gas[0]-cost[0];
-        for(int i=1; i<n; i++){
-            diff[i] = diff[i-1] + (gas[i] - cost[i]);
-        }
-        int ans = -1;
-        int firstPos = -1;
-        for(int i=n-1; i>=0; i--){
-            if(diff[i] < 0){
-                firstPos = i;
-                break;
-            }
-        }
-        
-        if(firstPos == -1) return 0;
-        if(firstPos == n-1) return -1;
-
-        int sum = 0;
-        boolean flag = false;
-        for(int i=firstPos; i>=0; i--){
-            if(gas[i]-cost[i] < 0 && !flag){
-                sum += (gas[i]-cost[i]);  
-                ans = i+1;
-                flag = true;              
-            }
-            else if(flag){
-                sum += (gas[i]-cost[i]);  
-            }
+        int sum=0, count=0;
+        int idx=-1;
+        boolean enter = false;
+        for(int i=0; i<2*n; i++){
            
-                
-                           
-            
-            if(sum >=0){
-                ans = i;
+            if(count==n) break;
+            if(sum+(gas[i%n]-cost[i%n]) >= 0){
+                sum += (gas[i%n]-cost[i%n]);
+                count++;
             }
+            else{
+                sum=0;
+                count=0;
+                idx=(i+1)%n;
+                enter = true;
+            }
+
+           
+
         }
-        return ans;
-        
+        if(count==n && !enter) return 0;
+        else if (count == n) return idx;
+        return -1;
+
         
     }
 }
