c3c53d51d6f1fc712c2539c2fbbd296c3256437c
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-07-23 16:42:45 +0530

    Time: 0 ms (100%), Space: 40.3 MB (76.54%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,6 +1,8 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int dp[][] = new int[m][n];
+        for(int i=0; i<m; i++)
+        Arrays.fill(dp[i], -1);
         int ways = paths(m-1, n-1,dp);
         return ways;
     }
@@ -9,7 +11,7 @@ class Solution {
         if(i<0 || j<0) return 0;
         if(i==0 && j==0)
         return dp[0][0]=1;
-        if(dp[i][j] != 0) return dp[i][j];
+        if(dp[i][j] != -1) return dp[i][j];
         return dp[i][j] = paths(i-1, j, dp)+paths(i, j-1, dp);
     }
 }
