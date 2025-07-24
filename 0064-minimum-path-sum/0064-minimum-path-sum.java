76488e51fb4df1ed8610d765afc6815d7ef2b055
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-07-24 15:58:57 +0530

    Time: 4 ms (60.67%), Space: 47.8 MB (43.52%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -3,21 +3,19 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         int dp[][] = new int[m][n];
-        for(int i=0; i<m; i++)
-        Arrays.fill(dp[i], -1);
-        return minSum(m-1, n-1, grid, dp);
-    }
+        dp[0][0] = grid[0][0];
+        for(int i=1; i<m; i++)
+        dp[i][0] = grid[i][0] + dp[i-1][0];
 
-    public int minSum(int i, int j, int grid[][], int dp[][]){
-        if(i==0 && j==0)
-        return dp[i][j]=grid[i][j];
-        else if(i<0 || j<0){
-            return Integer.MAX_VALUE;
+        for(int j=1; j<n; j++)
+        dp[0][j] = grid[0][j] + dp[0][j-1];
+
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                dp[i][j] = grid[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
+            }
         }
+        return dp[m-1][n-1];
 
-        if(dp[i][j] != -1)
-        return dp[i][j];
-        
-        return dp[i][j] = Math.min(minSum(i-1, j, grid, dp), minSum(i, j-1, grid, dp))+grid[i][j];
     }
 }
