48343d203a649891aaf9b6fabae45a1afe429d0c
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-07-29 20:46:35 +0530

    Time: 18 ms (52.76%), Space: 44.6 MB (33.7%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,29 +1,29 @@
 class Solution {
-    public int findTargetSumWays(int[] nums, int tar) {
+    public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
-        //calculate max target  
-        int sum =0;
+
+        int sum=0;
         for(int num: nums)
         sum += num;
 
-        if(tar<-sum || tar>sum) return 0;
+        if(target>sum || target<-sum) return 0;
 
         int dp[][] = new int[n+1][2*sum+1];
-        int offset =sum;
-        dp[0][0+offset] = 1;
+        int offset=sum;
+        dp[0][0+offset]=1;
 
         for(int i=1; i<n+1; i++){
             for(int j=-sum; j<=sum; j++){
-                int target = j+offset;
-                if(target-nums[i-1] >= 0 && target-nums[i-1] < (2*sum+1)){
-                    dp[i][target] += (dp[i-1][target-nums[i-1]]);
+                int idx=j+offset;
+            
+                if(idx+nums[i-1]>=0 && idx+nums[i-1] < 2*sum+1){
+                    dp[i][idx] += dp[i-1][idx+nums[i-1]];
                 }
-                if(target+nums[i-1]>=0 && target+nums[i-1] <(2*sum+1)){
-                    dp[i][target] += (dp[i-1][target+nums[i-1]]);
+                if(idx-nums[i-1] >= 0 && idx-nums[i-1]<2*sum+1){
+                    dp[i][idx] += dp[i-1][idx-nums[i-1]];
                 }
             }
         }
-
-        return dp[n][tar+offset];
+        return dp[n][target+offset];
     }
 }
