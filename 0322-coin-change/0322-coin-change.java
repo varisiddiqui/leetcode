36b43d8503e5c16b999bfd57263706b701aefa96
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-07-29 19:56:36 +0530

    Time: 16 ms (56.25%), Space: 45.5 MB (17.96%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -4,10 +4,6 @@ class Solution {
         int dp[][] = new int[n+1][amount+1];
 
         //dp[i][j] = min no. coins in first i-coins that can make j
-        dp[0][0] = 0;
-        for(int i=1; i<n+1; i++){
-            dp[i][0]=0;
-        }
 
         for(int j=1; j<amount+1; j++){
             dp[0][j] = Integer.MAX_VALUE-1;
