d8a7b0a724b20df7146a5398478f04aedb3ea4c9
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-08-02 10:34:41 +0530

    Time: 3 ms (62.39%), Space: 44.7 MB (45.72%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,33 +1,26 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
-         int n = prices.length;
-        int dp[][][]=new int[n][2][k+1];
-        for(int i=0; i<n; i++){
-            for(int j=0; j<2; j++){
-                Arrays.fill(dp[i][j], -1);
+        int n = prices.length;
+        int dp[][][]=new int[n+1][2][k+1];     
+
+        for(int i=n-1; i>=0; i--){
+            for(int buy=0; buy<=1; buy++){
+                for(int left=1; left<=k; left++){
+                    int profit=0;
+                    if(buy==1 && left>0){
+                        int take = -prices[i]+dp[i+1][0][left];
+                        int skip = 0+dp[i+1][1][left];
+                        profit = Math.max(take, skip);
+                    }
+                    else if(buy==0 && left>0){
+                        int take = prices[i]+dp[i+1][1][left-1];
+                        int skip = 0+dp[i+1][0][left];
+                        profit = Math.max(take, skip);
+                    }
+                    dp[i][buy][left] = profit;
+                }
             }
         }
-
-        return maxPft(0, 1, k, prices, dp);
-    }
-
-     public int maxPft(int i, int buy, int left, int prices[], int dp[][][]){
-        if(i==prices.length || left == 0) return 0;
-
-        if(dp[i][buy][left] != -1) return dp[i][buy][left];
-        int profit=0;
-        
-        if(buy==1 && left>0){
-            int take = -prices[i]+maxPft(i+1, 0, left, prices, dp);
-            int skip = 0 + maxPft(i+1, 1, left, prices, dp);
-            profit = Math.max(take, skip);
-        }
-        else if(buy==0 && left>0){
-            int take = prices[i]+maxPft(i+1, 1, left-1, prices, dp);
-            int skip = 0+maxPft(i+1, 0, left, prices, dp);
-            profit = Math.max(take, skip);
-        }
-
-        return dp[i][buy][left]=profit;
+        return dp[0][1][k];
     }
 }
