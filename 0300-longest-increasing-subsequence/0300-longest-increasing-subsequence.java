207864fac8b053abe50ad3d509cbfc52ef3bba48
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-08-03 16:21:39 +0530

    Time: 386 ms (10.9%), Space: 79.7 MB (10.06%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,33 +1,31 @@
 class Solution {
-    static int offset=1;
+    static int offset = 1;//coordinate changes for prev
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        if(n == 1) return 1;
         int dp[][] = new int[n][n+1];
+
         for(int i=0; i<n; i++)
         Arrays.fill(dp[i], -1);
-
-        return f(0, -1, nums, dp);
+        return maxL(0, -1, nums, dp);
         
     }
 
-    public int f(int curr_idx, int prev_idx, int nums[], int dp[][]){
-        if(curr_idx == nums.length) return 0;
+    public int maxL(int curr_idx, int prev_idx, int nums[], int dp[][]){
+        if(curr_idx == nums.length)
+        return 0;
 
         if(dp[curr_idx][prev_idx+offset] != -1) return dp[curr_idx][prev_idx+offset];
 
         int take=0;
         int skip=0;
-        
 
-        //take case 
-        if(prev_idx == -1 || nums[curr_idx]>nums[prev_idx]){
-            take = 1 + f(curr_idx+1, curr_idx, nums, dp);
+        //take
+        if(prev_idx == -1 || nums[curr_idx] > nums[prev_idx]){
+            take = 1 + maxL(curr_idx+1, curr_idx, nums, dp);
         }
 
-        //skip always available so try it
-        skip = f(curr_idx+1, prev_idx, nums, dp);
+        skip = 0 + maxL(curr_idx+1, prev_idx, nums, dp);
 
-        return dp[curr_idx][prev_idx+offset] = Math.max(take, skip);
+        return dp[curr_idx][prev_idx+offset]=Math.max(take, skip);
     }
 }
