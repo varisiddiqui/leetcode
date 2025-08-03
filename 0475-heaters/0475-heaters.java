2600197b78c6cc897bc877f2f378ac46184dc56e
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-08-03 12:23:35 +0530

    Time: 15 ms (83.07%), Space: 47.3 MB (19.6%) - LeetHub

--- 0475-heaters/0475-heaters.java
+++ 0475-heaters/0475-heaters.java
@@ -1,43 +1,46 @@
 class Solution {
     public int findRadius(int[] houses, int[] heaters) {
+        int house = houses.length;
+        int heater = heaters.length;
         Arrays.sort(houses);
         Arrays.sort(heaters);
-        int nhouse = houses.length;
-        int nheater = heaters.length;
-        int maxSingle=Integer.MIN_VALUE;
-        if(nheater==1){
-            for(int i=0; i<nhouse; i++)
-                maxSingle = Math.max(maxSingle, Math.abs(heaters[0]-houses[i]));
-                return maxSingle;
-        }
 
+        if(heater == 1) 
+        return Math.max(Math.abs(heaters[0]-houses[0]), Math.abs(heaters[0]-houses[house-1]));
+     
 
-        int max = Integer.MIN_VALUE;
-       
-            int i=0;
-            int l=0,r=1;
-            while(i<nhouse){
-                if(houses[i]<=heaters[l]){
-                    max = Math.max(max, Math.abs(houses[i]-heaters[l]));
-                    i++;
+        long maxR=0;
+
+        //for 2 or more heaters
+        int l=0, r=1, i=0;
+     
+        while(i<house){
+            if(houses[i]< heaters[l]){
+                maxR = Math.max(maxR, heaters[l] - houses[i]);
+                i++;
+            }
+            else if(heaters[l] <= houses[i] && houses[i] <= heaters[r]){
+                int minD = Math.min(Math.abs(houses[i] - heaters[l]), Math.abs(heaters[r] - houses[i]));
+                maxR = Math.max(maxR, minD);
+                i++;
+            }
+            else{
+                if(r+1 < heater){
+                    l++;
+                    r++;
                 }
-                else if(houses[i]>heaters[l] && houses[i]<=heaters[r]){
-                    int near = Math.min(Math.abs(houses[i]-heaters[l]), Math.abs(houses[i]-heaters[r]));
-                    max = Math.max(max, near);
-                    i++;
+                else{
+                    maxR = Math.max(maxR, Math.abs(houses[house-1] - heaters[r]));
+                    break;
+                    
                 }
-                else if(houses[i]>heaters[r]){
-                    if(r+1<nheater){
-                        l++;
-                        r++;
-                    }
-                    else{
-                        max = Math.max(max, houses[i]-heaters[nheater-1]);
-                        i++;
-                    }
-                }
+                
+            }
             
         }
-        return max;
+
+        return (int)maxR;
+
+        
     }
 }
