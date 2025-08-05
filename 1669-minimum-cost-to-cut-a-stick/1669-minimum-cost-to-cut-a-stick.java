d46665b3e01aa7e90775f855628d13555a52412f
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-08-06 00:18:30 +0530

    Time: 20 ms (69.79%), Space: 42.8 MB (27.48%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
@@ -1,33 +1,32 @@
 class Solution {
     public int minCost(int n, int[] cuts) {
-        int[] newCuts = new int[cuts.length + 2];
-        newCuts[0] = 0;
-        newCuts[newCuts.length - 1] = n;
-        System.arraycopy(cuts, 0, newCuts, 1, cuts.length);
-        Arrays.sort(newCuts);
-        int dp[][] = new int[cuts.length+2][cuts.length+2];
-        for(int i=0; i<cuts.length+2; i++)
-        Arrays.fill(dp[i], -1);
-        return mcm(0, newCuts.length-1, newCuts, dp);
+        ArrayList<Integer> cut = new ArrayList<>();
+        cut.add(0);
+        for(int i=0; i<cuts.length; i++)
+        cut.add(cuts[i]);
+        cut.add(n);
+        Collections.sort(cut);
 
-    }
+        int sz=cut.size();
 
-    public int mcm(int i, int j, int cuts[], int dp[][]){
-        if(j-i ==1)
-        return dp[i][j]=0;
-        if(dp[i][j] != -1)
-        return dp[i][j];
+        int dp[][] = new int[sz][sz];
 
-        int min = Integer.MAX_VALUE;
-
-        for(int k=i+1; k<j; k++){
-                int c1 = mcm(i, k, cuts, dp);
-                int c2 = mcm(k, j, cuts, dp);
-                int c3 = cuts[j]-cuts[i];
-                min = Math.min(min, c1+c2+c3);
-         }
-            return dp[i][j]=min;
+        for(int i=sz-1; i>=0; i--){
+            for(int j=i+1; j<sz; j++){
+                int min=Integer.MAX_VALUE;
+                for(int k=i+1; k<j; k++){
+                    int c1 = dp[i][k];
+                    int c2 = dp[k][j];
+                    int c3 = cut.get(j)-cut.get(i);
+                    min = Math.min(min, c1+c2+c3);
+                }
+                if(i+1 < j)
+                dp[i][j]=min;
+                else
+                dp[i][j]=0;
+            }
         }
-        
-    
+       
+        return  dp[0][sz-1];
+    }
 }
