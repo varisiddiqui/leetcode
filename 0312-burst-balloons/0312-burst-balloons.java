2946a14171a4eb85bd536446541214f91ee7e0a6
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-08-06 19:50:31 +0530

    Time: 29 ms (99.5%), Space: 42.6 MB (81.27%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -2,38 +2,37 @@ class Solution {
     public int maxCoins(int[] nums) {
         if(nums.length == 1)
         return nums[0];
+
         int a[] = new int[nums.length+2];
         a[0]=1;
         for(int i=0; i<nums.length; i++){
             a[i+1]=nums[i];
         }
         a[a.length-1]=1;
+        
         int dp[][] = new int[a.length][a.length];
+        int n = a.length;
         
-        for(int i=0; i<a.length; i++){
-            Arrays.fill(dp[i], -1);
+        for(int i=0; i<n; i++)
+        dp[i][i] = a[i];
+
+        for(int i=n-1; i>=0; i--){
+            for(int j=i+1; j<n; j++){
+                int min = 0;
+                for(int k=i+1; k<j; k++){
+                    int c1 = a[i]*a[k]*a[j];
+                    int c2 = dp[i][k];
+                    int c3 = dp[k][j];
+                    min = Math.max(min, c1+c2+c3);
+                }
+                dp[i][j]=min;
+            }
         }
 
-        return max(0, a.length-1, a, dp);
+        return dp[0][n-1];
         
         
     }
 
-    public int max(int i, int j, int a[], int dp[][]){
-        if(i == j)
-        return dp[i][j] = a[i];
-
-        if(dp[i][j] != -1)
-        return dp[i][j];
-        
-        int max = 0;
-
-        for(int k=i+1; k<j; k++){
-            int cost1 = a[i]*a[k]*a[j];
-            int cost2 = max(i, k, a, dp);
-            int cost3 = max(k, j, a, dp);
-            max = Math.max(max, cost1+cost2+cost3);
-        }
-        return dp[i][j]=max;
-    }
+   
 }
