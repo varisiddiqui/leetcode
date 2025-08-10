0aa31214c9ea61b33e1019b82675cacf8a6697a3
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-08-10 13:44:24 +0530

    Time: 184 ms (5.11%), Space: 45.6 MB (5.01%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.java
@@ -1,40 +1,46 @@
 class Solution {
     public int maxSumAfterPartitioning(int[] arr, int l) {
         int n = arr.length;
-        int dp[][] = new int[n][n];
-        for(int i=0; i<n; i++)
-        Arrays.fill(dp[i], -1);
-        return maxSum(0, n-1, arr, l, dp);
+        if(n==1)
+        return arr[0];
+        int dp[][] = new int[n][n]; 
+        
+        
+        
 
-    }
+        for(int i=n-1; i>=0; i--){
+            for(int j=i; j<n; j++){
+                int max=0;
 
-    public int maxSum(int i, int j, int arr[], int l, int dp[][]){
-        int max=0;
-        if(i>j)
-        return dp[i][j]=0;
+                if(j-i+1 <= l){
+                    for(int t=i; t<=j; t++)
+                        max = Math.max(max, arr[t]);
+                        dp[i][j]=(j-i+1)*max;
+                 }  
 
-        if(dp[i][j] != -1)
-        return dp[i][j];
+                 else{
+                    int localmax=0;
+                    for(int k=i; k<i+l; k++){
+                        localmax=Math.max(localmax, arr[k]);
+                            
+                             int left = (k-i+1)*localmax;
+                             int right = dp[k+1][j];
+                             int sum = left+right;
+                             max = Math.max(max, sum);
+                        }
+                        dp[i][j]=max;
+                 } 
 
-        if(j-i+1 <= l){
-            for(int t=i; t<=j; t++)
-            max = Math.max(max, arr[t]);
-
-            return dp[i][j]=(j-i+1)*max;
-        }     
-
-        for(int k=i; k<i+l; k++){
-            int localmax=0;
-            for(int q=i; q<=k; q++){
-                localmax = Math.max(localmax, arr[q]);
             }
-            int left = (k-i+1)*localmax;
-            int right = maxSum(k+1, j, arr, l, dp);
-            int sum = left+right;
-            max = Math.max(max, sum);
         }
-        return dp[i][j] = max;
+
+        return dp[0][n-1];
+        
+
+
 
 
     }
+
+    
 }
