5ac7df658fa05a699f25461c5cc0ca253e4bb095
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-08-10 12:38:06 +0530

    Time: 45 ms (5.68%), Space: 45.5 MB (5.01%) - LeetHub

---
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int maxSumAfterPartitioning(int[] arr, int l) {
+        int n = arr.length;
+        int dp[][] = new int[n][n];
+        for(int i=0; i<n; i++)
+        Arrays.fill(dp[i], -1);
+        return maxSum(0, n-1, arr, l, dp);
+
+    }
+
+    public int maxSum(int i, int j, int arr[], int l, int dp[][]){
+        int max=0;
+        if(i>j)
+        return dp[i][j]=0;
+
+        if(dp[i][j] != -1)
+        return dp[i][j];
+
+        if(j-i+1 <= l){
+            for(int t=i; t<=j; t++)
+            max = Math.max(max, arr[t]);
+
+            return dp[i][j]=(j-i+1)*max;
+        }     
+
+        for(int k=i; k<i+l; k++){
+            int localmax=0;
+            for(int q=i; q<=k; q++){
+                localmax = Math.max(localmax, arr[q]);
+            }
+            int left = (k-i+1)*localmax;
+            int right = maxSum(k+1, j, arr, l, dp);
+            int sum = left+right;
+            max = Math.max(max, sum);
+        }
+        return dp[i][j] = max;
+
+
+    }
+}
