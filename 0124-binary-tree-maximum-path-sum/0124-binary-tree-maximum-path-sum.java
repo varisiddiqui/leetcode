d7d7758a3b3e5e133b835a94e0dc422f4cb51ff9
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-08-14 17:54:11 +0530

    Time: 0 ms (100%), Space: 44.3 MB (77.78%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -15,42 +15,17 @@
  */
 class Solution {
     public int maxPathSum(TreeNode root) {
-        if(root == null)
-        return 0;
-        int max[] = new int[1];
-        max[0] = -10000;
-        sum(root, max);
-        return max[0];
+        int maxValue[] = new int[1];
+        maxValue[0]=Integer.MIN_VALUE;
+        maxP(root, maxValue);
+        return maxValue[0];
     }
 
-    public int sum(TreeNode root, int max[]){
-        if(root == null)
-        return 0;
-
-        int left = sum(root.left, max);
-        //left only
-        if(root.left != null)
-        max[0] = Math.max(left, max[0]);
-
-        int right = sum(root.right, max);
-        //right only
-        if(root.right != null)
-        max[0] = Math.max(right, max[0]);
-
-        //node only
-        max[0] = Math.max(max[0], root.val);
-
-        //left+node
-        max[0] = Math.max(max[0], left+root.val);
-
-        //right+node
-        max[0] = Math.max(max[0], right+root.val);
-
-        //overall
-        max[0] = Math.max(left+right+root.val, max[0]);
-
-        return root.val + Math.max(0, Math.max(left, right));
-
-
+    public int maxP(TreeNode root, int maxValue[]){
+        if(root == null) return 0;
+        int left = Math.max(0, maxP(root.left, maxValue));
+        int right = Math.max(0, maxP(root.right, maxValue));
+        maxValue[0] = Math.max(maxValue[0], left+right+root.val);
+        return Math.max(left, right)+root.val;
     }
 }
