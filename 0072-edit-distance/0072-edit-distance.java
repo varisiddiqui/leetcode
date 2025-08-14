f9959d089d6d155d585aadaddbc20629e0549572
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-08-14 12:18:13 +0530

    Time: 4 ms (72.86%), Space: 45.2 MB (30.53%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -6,25 +6,25 @@ class Solution {
         int dp[][] = new int[n1+1][n2+1];
         dp[0][0] = 0;
 
-        for(int i=1; i<n1+1; i++)
-        dp[i][0]=i;
-        for(int j=1; j<n2+1; j++)
-        dp[0][j]=j;
+        for(int i=0; i<n1; i++)
+        dp[i][n2]=n1-i;
+        for(int j=0; j<n2; j++)
+        dp[n1][j]=n2-j;
 
-        for(int i=1; i<n1+1; i++){
-            for(int j=1; j<n2+1; j++){
+        for(int i=n1-1; i>=0; i--){
+            for(int j=n2-1; j>=0; j--){
                 //matched
-                if(word1.charAt(i-1) == word2.charAt(j-1))
+                if(word1.charAt(i) == word2.charAt(j))
                 {
-                    dp[i][j] = dp[i-1][j-1];
+                    dp[i][j] = dp[i+1][j+1];
                 }
 
                 else{
-                    dp[i][j] = Math.min(Math.min(dp[i][j-1], dp[i-1][j]), dp[i-1][j-1])+1;
+                    dp[i][j] = Math.min(Math.min(dp[i][j+1], dp[i+1][j]), dp[i+1][j+1])+1;
                 }
             }
         }
 
-        return dp[n1][n2];
+        return dp[0][0];
     }
 }
