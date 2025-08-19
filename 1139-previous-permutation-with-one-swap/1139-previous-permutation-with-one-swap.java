f7437edfc18fb4b709d306cd5fffe95482cd94bb
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-08-19 18:18:19 +0530

    Time: 0 ms (100%), Space: 45.8 MB (69.23%) - LeetHub

--- 1139-previous-permutation-with-one-swap/1139-previous-permutation-with-one-swap.java
+++ 1139-previous-permutation-with-one-swap/1139-previous-permutation-with-one-swap.java
@@ -1,46 +1,32 @@
 class Solution {
     public int[] prevPermOpt1(int[] arr) {
         int n = arr.length;
-        Comparator<int[]> cmp = (a, b) -> {
-            if(a[0] != b[0]){
-                return b[0]-a[0];
-            }
-            else if(a[1] != b[1]){
-                return b[1]-a[1];
-            }
-            else{
-                return a[2]-b[2];
-            }
-        };
-        PriorityQueue<int[]> pq = new PriorityQueue<>(cmp);
 
-        int prevBig[] = new int[n];
-        Arrays.fill(prevBig, -1);
-
-        for(int i=n-1; i>=1; i--){
-            for(int j=i-1; j>=0; j--){
-                if(arr[j]>arr[i]){
-                    prevBig[i]=j;
-                    break;
+       int f=-1;
+    int p=-1;
+       for(int i=n-1; i>=1; i--){
+        if(arr[i-1]>arr[i]){
+            int min = Integer.MIN_VALUE;
+            p = i-1;
+            for(int j=i; j<n; j++){
+                if(arr[j]<arr[p] && min < arr[j]){
+                    min = arr[j];
+                    f=j;
+                    
                 }
             }
+            if(f!=-1) break;
         }
+       }
+
+       if(f == -1)
+       return arr;
+
+       int temp = arr[f];
+       arr[f] = arr[p];
+       arr[p]=temp;
 
 
-        for(int i=0; i<n; i++){
-            if(prevBig[i] != -1){
-                pq.add(new int[]{prevBig[i], arr[i], i});
-            }
-        }
-        if(pq.isEmpty()) return arr;
-        int a[] = pq.peek();
-
-        int toSwap = a[0];
-        int fromSwap = a[2];
-
-        int temp = arr[toSwap];
-        arr[toSwap] = arr[fromSwap];
-        arr[fromSwap] = temp;
 
         return arr;
 
