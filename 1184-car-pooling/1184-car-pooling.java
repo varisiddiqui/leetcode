cdb1bd4b69ba9aa0d0f80d96d3381af4766cd792
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-09-04 10:05:44 +0530

    Time: 1 ms (100%), Space: 44.7 MB (25.74%) - LeetHub

--- 1184-car-pooling/1184-car-pooling.java
+++ 1184-car-pooling/1184-car-pooling.java
@@ -3,29 +3,27 @@ class Solution {
         int max=0;
         int n = trips.length;
 
-        int l=10001;
-        int r=0;
+        int arr[] = new int[1001];
+
         for(int i=0; i<n; i++){
-            l = Math.min(l, trips[i][1]);
-            r = Math.max(r, trips[i][2]);
+            int pick = trips[i][1];
+            int drop = trips[i][2];
+            int pass = trips[i][0];
+            arr[pick] += pass;
+            arr[drop] -= pass;
         }
+
+        for(int i=0; i<1001; i++){
+            max += arr[i];
+            if(max>capacity)
+            return false;
+        }
+        return true;
+
+     
+       
+
         
-        for(int i=l; i<=r; i++){
-            int tot=0;
-            int hour=i;
-            for(int j=0; j<n; j++){
-                int x1 = trips[j][1];
-                int y1 = trips[j][2];
-                if(hour>=x1 && hour<y1)
-                tot += trips[j][0];               
-            }
-
-            max = Math.max(max, tot);
-        }
-
-        //System.out.println(max);
-
-        return max<=capacity;
 
 
     }
