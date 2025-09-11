a768fb220e6c1b82eba73e8d6003d76e5751c644
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-09-11 23:22:06 +0530

    Time: 90 ms (18.27%), Space: 58.2 MB (47.12%) - LeetHub

---
+++ 0901-advantage-shuffle/0901-advantage-shuffle.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int[] advantageCount(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+
+        int num1[][] = new int[n][2];
+        int num2[][] = new int[n][2];
+
+        int ans[] = new int[n];
+        Arrays.fill(ans, -1);
+
+        for(int i=0; i<n; i++){
+            num1[i][0] = nums1[i];
+            num1[i][1] = i;
+            num2[i][0] = nums2[i];
+            num2[i][1] = i;
+        }
+
+        Comparator<int[]> cmp = (a, b) -> {
+            return Integer.compare(a[0], b[0]);
+        };
+
+        Arrays.sort(num1, cmp);
+        Arrays.sort(num2, cmp);
+
+        List<Integer> s = new ArrayList<>();
+        
+        int i=0;
+        int j=0;
+        while(i<n && j<n){
+            if(num1[i][0] > num2[j][0]){
+                ans[num2[j][1]] = num1[i][0];
+                i++;
+                j++;
+            }
+            else{
+                s.add(num1[i][0]);
+                i++;
+            }
+        }
+
+        Collections.sort(s, Collections.reverseOrder());
+        int l=0;
+        for(int k=0; k<n; k++){
+            if(ans[k] == -1){
+                ans[k] = s.get(l++);
+            }
+        }
+
+        return ans;
+
+        
+        
+    }
+}
