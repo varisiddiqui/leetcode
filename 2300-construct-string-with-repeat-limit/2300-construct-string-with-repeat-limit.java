86bcdd2ce697f267ff9bf339cc86f98cc843fd86
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-09-16 10:28:23 +0530

    Time: 132 ms (6.02%), Space: 46 MB (24.44%) - LeetHub

--- 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.java
+++ 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.java
@@ -1,35 +1,17 @@
 class Solution {
-    static class Pair{
-        char ch;
-        int fr;
-        public Pair(char ch, int f){
-            this.ch = ch;
-            this.fr = f;
-        }
-    }
     public String repeatLimitedString(String s, int r) {
-        Map<Character, Integer> hm = new HashMap<>();
+        TreeMap<Character, Integer> tm = new TreeMap<>(Collections.reverseOrder());
         int n = s.length();
         for (int i = 0; i < n; i++) {
             char ch = s.charAt(i);
-            hm.put(ch, hm.getOrDefault(ch, 0) + 1);
+            tm.put(ch, tm.getOrDefault(ch, 0) + 1);
         }
         StringBuilder str = new StringBuilder("");
 
-        Comparator<Pair> cmp = (a, b) -> {
-            return b.ch-a.ch;
-        };
-
-        PriorityQueue<Pair> pq = new PriorityQueue<>(cmp);
-
-        for(Character key: hm.keySet()){
-            pq.add(new Pair(key, hm.get(key)));
-        }
-
-        while(!pq.isEmpty()){
-            char ch = pq.peek().ch;
-            int fr = pq.peek().fr;
-            pq.remove();
+        while(!tm.isEmpty()) {
+            char ch = tm.firstKey();
+            int fr = tm.get(ch);
+            tm.remove(ch);
 
             while (fr != 0) {
                 int mini = Math.min(fr, r);
@@ -37,13 +19,14 @@ class Solution {
                     str.append(ch);
                 fr -= mini;
                 if (fr > 0) {
-                    if (!pq.isEmpty()) {
-                        char c2 = pq.peek().ch;
-                        int f2 = pq.peek().fr;
-                        pq.remove();
+                    if (!tm.isEmpty()) {
+                        char c2 = tm.firstKey();
+                       
                         str.append(c2);
-                        if (f2 - 1 > 0)
-                            pq.add(new Pair(c2, f2-1));
+                        if (tm.get(c2) - 1 > 0)
+                            tm.put(c2, tm.get(c2) - 1);
+                        else
+                         tm.remove(c2);
 
                     } else {
                         return str.toString();
