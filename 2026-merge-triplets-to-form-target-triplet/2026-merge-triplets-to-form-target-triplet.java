60e07d600be7595e65a3e685a5b0ce11fd737214
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-09-27 06:44:59 +0530

    Time: 5 ms (68.19%), Space: 120.4 MB (60.36%) - LeetHub

--- 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.java
+++ 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.java
@@ -6,24 +6,24 @@ class Solution {
         if(n == 1 && Arrays.equals(triplets[0], target)){
             return true;
         }
-        Set<Integer> set = new HashSet<>();
-       
+        //Set<Integer> set = new HashSet<>();
+       int c1=0,c2=0,c3=0;
 
         for(int j=0; j<n; j++){
            
             if(triplets[j][0]>target[0] || triplets[j][1]> target[1] || triplets[j][2]> target[2] ){
-                set.add(j);
+                continue;
+            }
+            else{
+                if(c1 == 0 && triplets[j][0] == target[0]) c1=1;
+            if(c2 == 0 && triplets[j][1] == target[1]) c2=1;
+            if(c3 == 0 && triplets[j][2] == target[2]) c3=1;
             }
         }
 
-        int c1=0,c2=0,c3=0;
+        
 
-        for(int i=0; i<n; i++){
-            if(set.contains(i)) continue;
-            if(c1 == 0 && triplets[i][0] == target[0]) c1=1;
-            if(c2 == 0 && triplets[i][1] == target[1]) c2=1;
-            if(c3 == 0 && triplets[i][2] == target[2]) c3=1;
-        }
+       
 
 
         if(c1==1 && c2==1 && c3==1) return true;
