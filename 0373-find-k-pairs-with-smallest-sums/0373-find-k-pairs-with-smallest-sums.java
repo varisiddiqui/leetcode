3b4bda038182b1d83a5148e6d4ff1830ec9e6bf0
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-09-29 20:39:40 +0530

    Time: 57 ms (35.61%), Space: 56.4 MB (98.84%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -1,53 +1,33 @@
 class Solution {
-    static class Pair{
-        int idx1;
-        int idx2;
-        int sum;
-        public Pair(int n1, int n2, int sum){
-            this.idx1 = n1;
-            this.idx2 = n2;
-            this.sum = sum;
-        }
-    }
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        
-        int n1 = nums1.length;
-        int n2 = nums2.length;
+        int m = nums1.length;
+        int n = nums2.length;
 
         List<List<Integer>> ans = new ArrayList<>();
+        Set<Pair<Integer, Integer>> visited = new HashSet<>();
 
-        Comparator<Pair> cmp = (a, b) -> {
-            return a.sum-b.sum;
-        };
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b)->(a[0] - b[0]));
+        minHeap.offer(new int[]{nums1[0] + nums2[0], 0, 0});
+        visited.add(new Pair<Integer, Integer>(0, 0));
 
-        PriorityQueue<Pair> pq = new PriorityQueue<>(cmp);
-        Set<String> set = new HashSet<>();
+        while (k-- > 0 && !minHeap.isEmpty()) {
+            int[] top = minHeap.poll();
+            int i = top[1];
+            int j = top[2];
 
-        pq.add(new Pair(0, 0, nums1[0]+nums2[0]));
+            ans.add(List.of(nums1[i], nums2[j]));
 
-        while(!pq.isEmpty() && ans.size()<k){
-            Pair curr = pq.remove();
-            
-            ans.add(new ArrayList<>(Arrays.asList(nums1[curr.idx1], nums2[curr.idx2])));
+            if (i + 1 < m && !visited.contains(new Pair<Integer, Integer>(i + 1, j))) {
+                minHeap.offer(new int[]{nums1[i + 1] + nums2[j], i + 1, j});
+                visited.add(new Pair<Integer, Integer>(i + 1, j));
+            }
 
-            
-                if(curr.idx1+1<n1 && set.add((curr.idx1+1)+","+curr.idx2))
-                pq.add(new Pair(curr.idx1+1, curr.idx2, nums1[curr.idx1+1]+nums2[curr.idx2]));
-            
-            
-                if(curr.idx2+1<n2 && set.add(curr.idx1+","+(curr.idx2+1)))
-                pq.add(new Pair(curr.idx1, curr.idx2+1, nums1[curr.idx1]+nums2[curr.idx2+1]));
-            
-
+            if (j + 1 < n && !visited.contains(new Pair<Integer, Integer>(i, j + 1))) {
+                minHeap.offer(new int[]{nums1[i] + nums2[j + 1], i, j + 1});
+                visited.add(new Pair<Integer, Integer>(i, j + 1));
+            }
         }
 
-
-
-        
-
-       
-
         return ans;
-
     }
 }
