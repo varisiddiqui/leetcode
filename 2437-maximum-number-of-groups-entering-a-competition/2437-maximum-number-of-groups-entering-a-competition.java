dc4cb12c6e139b080dcb6b20e745c994d3cd2bea
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-10-02 13:07:51 +0530

    Time: 0 ms (100%), Space: 58.3 MB (27.82%) - LeetHub

--- 2437-maximum-number-of-groups-entering-a-competition/2437-maximum-number-of-groups-entering-a-competition.java
+++ 2437-maximum-number-of-groups-entering-a-competition/2437-maximum-number-of-groups-entering-a-competition.java
@@ -1,14 +1,6 @@
 class Solution {
     public int maximumGroups(int[] grades) {
         int n = grades.length;
-        int limit=1,i=2;
-        int gp=0;
-        while(limit<=n){
-            limit = i*(i+1)/2;
-            i++;
-            gp++;
-        }
-       
-        return gp;
+        return (int)((Math.sqrt(1 + 8L * n) - 1) / 2);
     }
 }
