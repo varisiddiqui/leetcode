1e836329ee4c2d6b9760d1f911cf24d4a8a55ea8
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-10-08 11:47:47 +0530

    Time: 427 ms (6.77%), Space: 47.8 MB (5.26%) - LeetHub

---
+++ 1824-maximum-number-of-eaten-apples/1824-maximum-number-of-eaten-apples.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int eatenApples(int[] apples, int[] days) {
+        int n = days.length;
+        Comparator<int[]> cmp = (a, b) -> {
+            return a[1] - b[1];
+        };
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(cmp);
+        int time = 1;
+        int ans = 0;
+        int curr[] = new int[2];
+
+        for (int i = 0; i < n; i++) {
+
+            if (apples[i] != 0)
+                pq.add(new int[] { apples[i], i + days[i] });
+
+            while (!pq.isEmpty() && pq.peek()[1] < time) {
+                //System.out.println(pq.peek()[0] + " " + pq.peek()[1]);
+                pq.remove();
+            }
+
+            if (!pq.isEmpty()) {
+                curr = pq.remove();
+                ans += 1;
+                System.out.println(curr[0] + " " + curr[1]);
+            } else
+                curr = new int[2];
+
+            if (curr[0] - 1 > 0) {
+                curr[0] -= 1;
+                pq.add(curr);
+            }
+
+            time++;
+        }
+
+        while (!pq.isEmpty()) {
+            while (!pq.isEmpty() && pq.peek()[1] < time) {
+                //System.out.println(pq.peek()[0] + " " + pq.peek()[1]);
+                pq.remove();
+            }
+
+            if (!pq.isEmpty()) {
+                ans += 1;
+                curr = pq.remove();
+            }
+
+            else
+                curr = new int[2];
+
+           
+            if (curr[0] - 1 > 0) {
+                curr[0] -= 1;
+                pq.add(curr);
+            }
+
+            time++;
+        }
+
+        return ans;
+    }
+}
