0613b135a5abd121722f54b97b5511c780f98475
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-10-23 21:16:37 +0530

    Time: 8 ms (49.13%), Space: 44.9 MB (80.77%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,25 +1,29 @@
-import java.util.*;
-class  Solution{
+class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i=0;i<nums.length-1;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(i==j){
-                    continue;
-                }
-                else{
-                    if((nums[i]+nums[j])==target){
-                        int c[]={i,j};
-                        return c;
+        int n = nums.length;
+        int nums2[][] = new int[n][2];
 
-                    }
-                }
-            }
+        Comparator<int[]> cmp = (a, b) -> {
+            return a[0]-b[0];
+        };
+
+        for(int i=0; i<n; i++){
+            nums2[i][0]=nums[i];
+            nums2[i][1]=i;
         }
-        return null;
-    }
-}
-       
+        Arrays.sort(nums2, cmp);
+
+        
+        int i=0;
+        int j=n-1;
+
+        while(i<n && j>=0 && (nums2[i][0]+nums2[j][0]) != target){
+            if(nums2[i][0]+nums2[j][0]<target) i++;
+            else j--;
+        }
+
         
 
-       
-    
+        return new int[]{nums2[i][1], nums2[j][1]};
+    }
+}
