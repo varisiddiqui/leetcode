291866d3f68eae24877f07054948784eee23d4c8
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-12-11 15:31:32 +0530

    Time: 3 ms (85.79%), Space: 52.2 MB (42.54%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,39 +1,38 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int row = grid.length;
-        int col = grid[0].length;
-        
-        boolean vis[][] = new boolean[row][col];
-        
-        int ans=0;
-        
-        for(int i=0; i<row; i++){
-            for(int j=0; j<col; j++){
-                if(!vis[i][j] && grid[i][j]== '1'){
-                    ans++;
-                    dfs(i, j, grid, vis);
+        int r = grid.length;
+        int c = grid[0].length;
+
+        boolean vis[][] = new boolean[r][c];
+        int count=0;
+
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                
+                if(grid[i][j]=='1' && !vis[i][j]){
+                    dfs(i, j, vis, grid);
+                    count++;
                 }
             }
         }
-        return ans;
+        return count;
     }
 
-     public void dfs(int i, int j, char grid[][], boolean vis[][]){
-        int row = grid.length;
-        int col = grid[0].length;
-        
-        if(i<0 || i>=row || j<0 || j>=col || grid[i][j]=='0' || vis[i][j]) return;
-        
+    public void dfs(int i, int j, boolean vis[][], char graph[][]){
+        if(graph[i][j] == '0' || vis[i][j]) return;
+
         vis[i][j]=true;
-        
-        
-        dfs(i+1, j, grid, vis);
-        dfs(i-1, j, grid, vis);
-        dfs(i, j+1, grid, vis);
-        dfs(i, j-1, grid, vis);
-       
-        
-        
-        
+
+        if(i+1<graph.length)
+        dfs(i+1, j, vis, graph);
+        if(i-1>=0)
+        dfs(i-1, j, vis, graph);
+        if(j+1<graph[0].length)
+        dfs(i, j+1, vis, graph);
+        if(j-1>=0)
+        dfs(i, j-1, vis, graph);
     }
+
+    
+
 }
