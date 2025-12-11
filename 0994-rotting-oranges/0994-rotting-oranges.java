c7bf74266558c29f2262113033c195516e0fb3fa
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-12-11 18:28:40 +0530

    Time: 2 ms (82.15%), Space: 43.9 MB (62.6%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -5,33 +5,30 @@ class Solution {
 
         //boolean vis[][] = new boolean[r][c];
         Queue<int[]> q = new LinkedList<>();
-
+        int freshCount=0;
         for(int i=0; i<r; i++){
             for(int j=0; j<c; j++){
                 if(graph[i][j] == 2) 
                 q.add(new int[]{i, j, 0});
+                else if(graph[i][j] == 1) freshCount++;
             }
         }
 
         int count=0;
 
-        count = bfs(q, graph);
+        count = bfs(q, graph, freshCount);
 
-        for(int i=0; i<r; i++){
-            for(int j=0; j<c; j++){
-                if(graph[i][j] == 1) return -1;
-                
-            }
-        }
+       
 
         return count;
         
     }
 
-    public int bfs(Queue<int[]> q, int graph[][]){
+    public int bfs(Queue<int[]> q, int graph[][], int freshCount){
         int r = graph.length;
         int c = graph[0].length;
         int max=0;
+        int fresh=0;
 
         while(!q.isEmpty()){
             int arr[] = q.remove();
@@ -44,26 +41,30 @@ class Solution {
             if(i+1<r && graph[i+1][j] == 1){
                 graph[i+1][j]=2;
                 q.add(new int[]{i+1, j, arr[2]+1});
+                fresh++;
             }
 
             if(i-1>=0 && graph[i-1][j] == 1){
                 graph[i-1][j]=2;
                 q.add(new int[]{i-1, j, arr[2]+1});
+                fresh++;
             }
 
             if(j+1<c && graph[i][j+1] == 1){
                 graph[i][j+1]=2;
                 q.add(new int[]{i, j+1, arr[2]+1});
+                fresh++;
             }
 
             if(j-1>=0 && graph[i][j-1]==1){
                 graph[i][j-1] = 2;
                 q.add(new int[]{i, j-1, arr[2]+1});
+                fresh++;
             }
 
 
         }
 
-        return max;
+        return (fresh == freshCount)? max: -1;
     }
 }
