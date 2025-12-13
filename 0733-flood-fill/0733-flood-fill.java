429de583a46cf6c7ee9c3f6e7bb55861d5b02f09
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-12-13 18:00:50 +0530

    Time: 1 ms (44.3%), Space: 46.6 MB (61.21%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int r = image.length;
+        int c = image[0].length;
+        int org = image[sr][sc];
+
+        boolean vis[][] = new boolean[r][c];
+    
+                dfs(vis, org, image, sr, sc, color);
+        
+        
+        return image;
+    }
+
+    public void dfs(boolean vis[][], int org, int image[][], int i, int j, int color){
+       
+        vis[i][j]=true;
+        
+        int r = image.length;
+        int c = image[0].length;
+
+        image[i][j]=color;
+        if(i+1<r && image[i+1][j] == org && !vis[i+1][j]){
+            dfs(vis, org, image, i+1, j, color);
+        }
+
+        if(i-1>=0 && image[i-1][j] == org && !vis[i-1][j]){
+            dfs(vis,org, image, i-1, j, color);
+        }
+
+        if(j+1<c && image[i][j+1] == org && !vis[i][j+1]){
+            dfs(vis, org, image, i, j+1, color);
+        }
+
+        if(j-1>=0 && image[i][j-1]==org && !vis[i][j-1]){
+            dfs(vis, org, image, i, j-1, color);
+        }
+    }
+}
