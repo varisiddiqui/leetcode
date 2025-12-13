a72760b6e1a9341cd0c0e9cfa68fe0e6e879562b
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-12-13 18:56:41 +0530

    Time: 0 ms (100%), Space: 46.6 MB (52.54%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -4,36 +4,36 @@ class Solution {
         int c = image[0].length;
         int org = image[sr][sc];
 
-        boolean vis[][] = new boolean[r][c];
+        //boolean vis[][] = new boolean[r][c];
     
-                dfs(vis, org, image, sr, sc, color);
+                dfs(org, image, sr, sc, color);
         
         
         return image;
     }
 
-    public void dfs(boolean vis[][], int org, int image[][], int i, int j, int color){
+    public void dfs(int org, int image[][], int i, int j, int color){
        
-        vis[i][j]=true;
+   
         
         int r = image.length;
         int c = image[0].length;
 
         image[i][j]=color;
-        if(i+1<r && image[i+1][j] == org && !vis[i+1][j]){
-            dfs(vis, org, image, i+1, j, color);
+        if(i+1<r && image[i+1][j] == org&& image[i+1][j] != color){
+            dfs(org, image, i+1, j, color);
         }
 
-        if(i-1>=0 && image[i-1][j] == org && !vis[i-1][j]){
-            dfs(vis,org, image, i-1, j, color);
+        if(i-1>=0 && image[i-1][j] == org && image[i-1][j] != color){
+            dfs(org, image, i-1, j, color);
         }
 
-        if(j+1<c && image[i][j+1] == org && !vis[i][j+1]){
-            dfs(vis, org, image, i, j+1, color);
+        if(j+1<c && image[i][j+1] == org&& image[i][j+1] != color){
+            dfs(org, image, i, j+1, color);
         }
 
-        if(j-1>=0 && image[i][j-1]==org && !vis[i][j-1]){
-            dfs(vis, org, image, i, j-1, color);
+        if(j-1>=0 && image[i][j-1]==org&& image[i][j-1] != color){
+            dfs(org, image, i, j-1, color);
         }
     }
 }
