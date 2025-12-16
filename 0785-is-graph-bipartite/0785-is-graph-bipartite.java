10d2dd52a02a1ceefb6026c2d84a309964a11c80
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-12-16 10:20:08 +0530

    Time: 0 ms (100%), Space: 47.2 MB (22.24%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -2,11 +2,11 @@ class Solution {
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
        
-        boolean vis[] = new boolean[n];
+        //boolean vis[] = new boolean[n];
         int color[] = new int[n];
         for(int i=0; i<n; i++){
-            if(!vis[i]){
-                if(!dfs(i, -1, graph, color, vis))
+            if(color[i] == 0){
+                if(!dfs(i, -1, graph, color))
                 return false;
             }
         }
@@ -14,19 +14,19 @@ class Solution {
         
   }
 
-  public boolean dfs(int curr, int par, int graph[][], int color[], boolean vis[]){
+  public boolean dfs(int curr, int par, int graph[][], int color[]){
     if(par == -1){
         color[curr] = 1;
     }
     else if(color[par] == 1) color[curr]=2;
     else color[curr]=1;
 
-    vis[curr]=true;
+    //vis[curr]=true;
     for(int i=0; i<graph[curr].length; i++){
-        if(!vis[graph[curr][i]]){
-            if(!dfs(graph[curr][i], curr, graph, color, vis)) return false;
+        if(color[graph[curr][i]] == 0){
+            if(!dfs(graph[curr][i], curr, graph, color)) return false;
         }
-        else if(vis[graph[curr][i]] && graph[curr][i] != par &&  color[graph[curr][i]] == color[curr]) return false;
+        else if((color[graph[curr][i]] == 1 || color[graph[curr][i]] == 2) && graph[curr][i] != par &&  color[graph[curr][i]] == color[curr]) return false;
     }
     return true;
   }
