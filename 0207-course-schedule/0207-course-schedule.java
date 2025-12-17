942d98a803c94e0b675a9864d1a66e051aaea9ed
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-12-17 20:08:10 +0530

    Time: 4 ms (92.2%), Space: 47.2 MB (23.28%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean canFinish(int v, int[][] grid) {
+        @SuppressWarnings("unchecked")
+        List<Integer> graph[] = new ArrayList[v];
+        Arrays.setAll(graph, i -> new ArrayList<>());
+        for(int i=0; i<grid.length; i++){
+            int src = grid[i][0];
+            int des = grid[i][1];
+            graph[src].add(des);
+        }
+
+        boolean vis[] = new boolean[v];
+        boolean pathVis[] = new boolean[v];
+
+        for(int i=0; i<v; i++){
+            if(!vis[i]){
+                if(isCycle(i, vis, pathVis, graph)) return false;
+            }
+        }
+        return true;        
+        
+        
+    }
+
+    public boolean isCycle(int curr, boolean vis[], boolean pathVis[], List<Integer> graph[]){
+        vis[curr] = true;
+        pathVis[curr] = true;
+
+        List<Integer> li = graph[curr];
+
+        for(int i=0; i<li.size(); i++){
+            if(!vis[li.get(i)]){
+                if(isCycle(li.get(i), vis, pathVis, graph)) return true;
+            }
+            else if(pathVis[li.get(i)]) return true;
+        }
+        pathVis[curr] = false;
+        return false;
+    }
+}
