63bc98dc63fb9f153fd755357b80d610c9a0746d
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2025-12-27 19:56:23 +0530

    Time: 12 ms (62.29%), Space: 50.7 MB (10.76%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,60 +1,70 @@
 class Solution {
 
-    static class Pair{
+    static class Pair {
         int des;
         int wt;
-        
-        public Pair(int des, int wt){
+
+        public Pair(int des, int wt) {
             this.des = des;
             this.wt = wt;
         }
     }
 
+    static class Node {
+        int dist;
+        int v;
+
+        Node(int dist, int v) {
+            this.dist = dist;
+            this.v = v;
+        }
+    }
+
     public int networkDelayTime(int[][] edges, int v, int src) {
-
         @SuppressWarnings("unchecked")
-        List<Pair> graph[] = new ArrayList[v+1];
-
+        List<Pair>[] graph = new ArrayList[v+1];
         Arrays.setAll(graph, i -> new ArrayList<>());
-        
-        for(int i=0; i<edges.length; i++){
+
+        // build graph
+        for (int i = 0; i < edges.length; i++) {
             int s = edges[i][0];
             int des = edges[i][1];
             int wt = edges[i][2];
-            
+
             graph[s].add(new Pair(des, wt));
-            //graph[des].add(new Pair(s, wt));
+         
         }
-        
-        int dist[] = new int[v+1];
+
+        int[] dist = new int[v+1];
         Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[src]=0;
-        
-        Comparator<int[]> cmp = (a, b) -> {
-            if(a[0] != b[0]) return Integer.compare(a[0], b[0]);
-            else return Integer.compare(a[1], b[1]);
-        };
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>(cmp);
-        
-        pq.add(new int[]{0, src});
-        
-        while(!pq.isEmpty()){
-            int top[] = pq.remove();
-            int s = top[1];
-            
-            List<Pair> li = graph[s];
-            
-            for(Pair neigh: li){
-                if(dist[s]+neigh.wt < dist[neigh.des]){
-                    dist[neigh.des] = dist[s]+neigh.wt;
-                    pq.add(new int[]{dist[neigh.des], neigh.des});
+        dist[src] = 0;
+
+        // TreeSet comparator (distance first, node second)
+        TreeSet<Node> set = new TreeSet<>(
+                (a, b) -> a.dist != b.dist ? a.dist - b.dist : a.v - b.v);
+
+        set.add(new Node(0, src));
+
+        while (!set.isEmpty()) {
+
+            Node top = set.pollFirst(); // smallest distance
+            int s = top.v;
+
+            for (Pair neigh : graph[s]) {
+
+                if (dist[s] + neigh.wt < dist[neigh.des]) {
+
+                    // remove old entry if exists
+                    if (dist[neigh.des] != Integer.MAX_VALUE) {
+                        set.remove(new Node(dist[neigh.des], neigh.des));
+                    }
+
+                    dist[neigh.des] = dist[s] + neigh.wt;
+                    set.add(new Node(dist[neigh.des], neigh.des));
                 }
             }
-            
         }
-        
-        int max = Integer.MIN_VALUE;
+         int max = Integer.MIN_VALUE;
 
         for(int i=1; i<v+1; i++){
             int num = dist[i];
@@ -62,6 +72,8 @@ class Solution {
             max = Math.max(max, num);
         }
         return max;
+
         
+
     }
 }
