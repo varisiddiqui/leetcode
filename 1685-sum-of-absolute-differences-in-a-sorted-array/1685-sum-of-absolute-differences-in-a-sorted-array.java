ba67adfe00bf80f5199f9a98e5e797ee65b465f6
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-01-10 00:22:44 +0530

    Time: 5 ms (36.95%), Space: 85.5 MB (65.98%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
@@ -1,23 +1,26 @@
 class Solution {
     public int[] getSumAbsoluteDifferences(int[] nums) {
         int n = nums.length;
-        int prefix[] = new int[n];
-        prefix[0]=nums[0];
-        for(int i=1; i<n; i++){
-            prefix[i] = prefix[i-1]+nums[i];
+        int suffix=0;
+        int prefix=0;
+        
+        for(int i=0; i<n; i++){
+           suffix += nums[i];
         }
 
         int diff[] = new int[n];
        // diff[0] = prefix[1]-((n-1)*nums[0])
         for(int i=0; i<n; i++){
+            suffix -= nums[i];
             if(i-1>=0){
-                diff[i] += (i*nums[i] - prefix[i-1]);
-                System.out.print(diff[i]+" ");
+                diff[i] += (i*nums[i] - prefix);
+               
             }
             if(i+1<n){
-                diff[i] += ((prefix[n-1]-prefix[i]) - ((n-i-1)*nums[i]));
-                System.out.print(diff[i]+" ");
+                diff[i] += (suffix - ((n-i-1)*nums[i]));
+              
             }
+            prefix += nums[i];
   
         }
         return diff;
