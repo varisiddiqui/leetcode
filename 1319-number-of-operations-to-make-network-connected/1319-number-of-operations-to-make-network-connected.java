221ad85c48839106ad226079187c4262fa16fb11
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-01-19 19:07:05 +0530

    Time: 8 ms (53.34%), Space: 69.2 MB (33.17%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -0,0 +1,57 @@
+class Solution {
+    
+    public int makeConnected(int n, int[][] connections) {
+        int par[] = new int[n];
+        int sz[] = new int[n];
+
+        for(int i=0; i<n; i++){
+            par[i]=i;
+            sz[i]=1;
+        }
+        int extra=0;
+
+        for(int i=0; i<connections.length; i++){
+            int x = connections[i][0];
+            int y = connections[i][1];
+
+            if(find(par, x) == find(par, y)){
+                extra++;
+                continue;
+            }
+            else{
+                union(par, sz, x, y);
+            }
+        }
+
+        Set<Integer> set = new HashSet<>();
+        for(int i=0; i<n; i++){
+            set.add(find(par, i));
+        }
+
+        return (extra<(set.size()-1))?-1: set.size()-1;
+    }
+
+    static int find(int par[], int x){
+        if(x == par[x]) return x;
+        else return par[x] = find(par, par[x]);
+    }
+
+    static void union(int par[], int sz[], int x, int y){
+        int parX = find(par, x);
+        int parY = find(par, y);
+
+        if(parX == parY) {
+           
+            return;
+        }
+
+        if(sz[parX] < sz[parY]){
+            par[parX] = parY;
+            sz[parY] += sz[parX];
+        }
+        else{
+            par[parY] = parX;
+            sz[parX] += sz[parY];
+        }
+    }
+}
