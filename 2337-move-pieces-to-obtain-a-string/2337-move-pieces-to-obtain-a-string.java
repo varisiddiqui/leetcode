390590cda9fd1dfc8c6dc56dae2913764c38e493
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-01-22 17:02:40 +0530

    Time: 12 ms (94.27%), Space: 47.9 MB (63.69%) - LeetHub

--- 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.java
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.java
@@ -1,105 +1,24 @@
 class Solution {
     public boolean canChange(String start, String target) {
-       
-        List<Integer> idxS = new ArrayList<>();
-        List<Integer> idxT = new ArrayList<>();
+         int n = start.length();
+        int i = 0, j = 0;
 
-        int n1 = start.length();
-        int n2 = target.length();
+        while (i < n || j < n) {
+            while (i < n && start.charAt(i) == '_') i++;
+            while (j < n && target.charAt(j) == '_') j++;
 
-        for(int i=0; i<n1; i++){
-            if(start.charAt(i) != '_') idxS.add(i);
-        }
-        for(int i=0; i<n2; i++){
-            if(target.charAt(i) != '_') idxT.add(i);
-        }
+            if (i == n || j == n) return i == n && j == n;
 
+            char a = start.charAt(i);
+            char b = target.charAt(j);
+            if (a != b) return false;
 
-        if(idxS.size() != idxT.size()) return false;
-        int n = idxS.size();
+            if (a == 'L' && i < j) return false; // L can only move left
+            if (a == 'R' && i > j) return false; // R can only move right
 
-        //checking from left for L
-        int i=0;
-        int j=0;
-        int cnt=n;
-
-        while(i<n && j<n){
-            //got L
-            while(j<n && target.charAt(idxT.get(j)) != 'L') j++; //got L at j
-            if(j == n) break;
-
-
-            while(i<n && start.charAt(idxS.get(i)) != 'L') i++; //got L in i
-            if(i==n) return false;
-
-            //System.out.println(i+" "+j);
-
-            if(idxT.get(j) == idxS.get(i)) {
-                cnt--;
-            }
-
-            else if(idxT.get(j) > idxS.get(i)) return false;
-
-            else {
-                if(i==0) 
-                {
-                    cnt--;
-                    idxS.set(i, idxT.get(j));
-                }
-                else if(idxS.get(i-1) >= idxT.get(j)){
-                    return false;
-                }
-                else{
-                    cnt--;
-                    idxS.set(i, idxT.get(j));
-                }
-
-            }
             i++;
             j++;
-            
         }
-
-        i=n-1;
-        j=n-1;
-
-        while(i>=0 && j>=0){
-            //got R
-            while(j>=0 && target.charAt(idxT.get(j)) != 'R') j--; //got R at j
-            if(j == -1) break;
-
-            while(i>=0 && start.charAt(idxS.get(i)) != 'R') i--; //got R in i
-            if(i==-1) return false;
-
-            if(idxT.get(j) == idxS.get(i)) {
-                cnt--;
-            }
-
-            else if(idxT.get(j) < idxS.get(i)) return false;
-
-            else {
-                if(i==n-1){
-                    cnt--;
-                    idxS.set(i, idxT.get(j));
-                } 
-                else if(idxS.get(i+1) <= idxT.get(j)){
-                    return false;
-                }
-                else{
-                    cnt--;
-                    idxS.set(i, idxT.get(j));
-                }
-
-            }
-            i--;
-            j--;
-        }
-        if(cnt == 0) return true;
-        return false;
-        
-        
-              
-
-        
+        return true;
     }
 }
