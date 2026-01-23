3a816a28031680e13b42645cdea1cd1432f37514
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-01-23 11:51:58 +0530

    Time: 4 ms (58%), Space: 52.7 MB (43.8%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,31 +1,45 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n = nums.length;
-        int c1=0, c2=0, m1=0, m2=0;
+        int c1=0;
+        int num1=-1;
+
+        int c2=0;
+        int num2=-1;
+
         for(int i=0; i<n; i++){
-            if(m1 == nums[i]) c1++;
-            else if(m2 == nums[i]) c2++;
-            else if(c1 == 0){
-                c1 = 1;
-                m1 = nums[i];
+            if(c1 == 0 && nums[i] != num2){
+                c1++;
+                num1 = nums[i];
             }
-            else if(c2 == 0){
-                c2 = 1;
-                m2 = nums[i];
+            else if(c2 == 0 && nums[i] != num1){
+                c2++;
+                num2 = nums[i];
             }
-            else{
+            else if(nums[i] == num1) c1++;
+            else if(nums[i] == num2) c2++;
+            else {
                 c1--;
                 c2--;
             }
         }
-        List<Integer> al = new ArrayList<>();
-        int c11=0, c22=0;
-        for(int i=0; i<n; i++){
-            if(m1 == nums[i]) c11++;
-            else if(m2 == nums[i]) c22++;
+        int limit = (int)Math.floor(n/3);
+        int count1=0;
+        int count2=0;
+        for(int num: nums){
+            if(num == num1) count1++;
+            if(num == num2) count2++;
         }
-        if(c11>(n/3)) al.add(m1);
-        if(c22>(n/3)) al.add(m2);
-        return al;
+        if(count1 == n) return new ArrayList<>(Arrays.asList(num1));
+        if(count2 == n) return new ArrayList<>(Arrays.asList(num2));
+        
+        if(count1>limit && count2>limit){
+            return new ArrayList<>(Arrays.asList(num1, num2));
+        }
+        else if(count1>limit) return new ArrayList<>(Arrays.asList(num1));
+        else if(count2>limit) return new ArrayList<>(Arrays.asList(num2));
+        return new ArrayList<>();
+        
+
     }
 }
