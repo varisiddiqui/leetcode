1cc63960065929f4ee8655aacc8b31cc7bc0ea6e
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-02-09 16:48:25 +0530

    Time: 0 ms (100%), Space: 43.5 MB (82.83%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -17,37 +17,18 @@ class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
 
-        if(root == null) return ans;
-
-        TreeMap<Integer, List<int[]>> tm = new TreeMap<>();
-
-        int r =0;
-        int col = 0;
-
-        postOrder(tm, root, r, col);
-
-        Comparator<int[]> cmp = (a, b) -> {
-            return Integer.compare(b[1], a[1]);
-        };
-
-        for(Integer row: tm.keySet()){
-            
-            ans.add(tm.get(row).get(0)[0]);
-        }
-
-
+        int level=0;
+        postOrder(root, level, ans);
         return ans;
 
     }
 
-    public void postOrder(TreeMap<Integer, List<int[]>> tm, TreeNode root, int row, int col){
+    public void postOrder(TreeNode root, int level, List<Integer> ans){
         if(root == null) return;
 
-        tm.putIfAbsent(row, new ArrayList<>());
+        if(level == ans.size()) ans.add(root.val);
 
-        tm.get(row).add(new int[]{root.val, col});
-
-        postOrder(tm, root.right, row+1, col+1);
-        postOrder(tm, root.left, row+1, col-1);
+        postOrder(root.right, level+1, ans);
+        postOrder(root.left, level+1, ans);
     }
 }
