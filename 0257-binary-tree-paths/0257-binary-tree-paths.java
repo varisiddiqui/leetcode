342b168a09a8150a5abc3501e7d4497ad7517f19
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-02-09 17:26:18 +0530

    Time: 3 ms (76.45%), Space: 44.3 MB (66.97%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> ans = new ArrayList<>();
+        if(root == null) return ans;
+
+        String rootD = String.valueOf(root.val);
+        StringBuilder str = new StringBuilder(rootD);
+
+        if(root.left == null && root.right==null){
+            ans.add(str.toString());
+            return ans;
+        }
+
+        
+
+        postOrder(root.left, ans, new StringBuilder(str.toString()));
+        postOrder(root.right, ans, new StringBuilder(str.toString()));
+
+        return ans;
+
+    }
+
+    public void postOrder(TreeNode root, List<String> ans, StringBuilder str){
+        if(root == null) return;
+
+        String rootD = String.valueOf(root.val);
+        rootD = "->"+rootD;
+        str.append(rootD);
+
+        if(root.left == null && root.right==null){
+            ans.add(str.toString());
+            return;
+        }
+
+        
+
+       postOrder(root.left, ans, new StringBuilder(str.toString()));
+        postOrder(root.right, ans, new StringBuilder(str.toString()));
+
+
+    }
+}
