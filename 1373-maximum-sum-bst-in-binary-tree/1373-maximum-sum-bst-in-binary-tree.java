e39841e1ca86386a379559271c450105e8ee4fba
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-03-11 09:46:07 +0530

    Time: 7 ms (53.56%), Space: 67.1 MB (18.34%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.java
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.java
@@ -1,51 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-
+    int globalMax=0;
     static class NodeValue {
         int min, max, sum;
-        boolean isBST;
 
-        public NodeValue(int min, int max, int sum, boolean isBST) {
-            this.min = min;
+        public NodeValue(int max, int min, int sum) {
             this.max = max;
+            this.min = min;
             this.sum = sum;
-            this.isBST = isBST;
         }
     }
 
-    private int maxSum = 0;
-
     public NodeValue largestBST(TreeNode root) {
-        // Base case: null nodes are valid BSTs
         if (root == null) {
-            return new NodeValue(Integer.MAX_VALUE, Integer.MIN_VALUE, 0, true);
+            return new NodeValue(Integer.MIN_VALUE, Integer.MAX_VALUE, 0);
         }
 
         NodeValue left = largestBST(root.left);
         NodeValue right = largestBST(root.right);
 
-        // A node is a BST if:
-        // 1. Both children are BSTs
-        // 2. The root value is greater than the maximum in the left subtree
-        // 3. The root value is smaller than the minimum in the right subtree
-        if (left.isBST && right.isBST && left.max < root.val && root.val < right.min) {
-            int currentSum = left.sum + right.sum + root.val;
-            maxSum = Math.max(maxSum, currentSum);
-            
+        if (left.max < root.val && root.val < right.min) {
+            //may be null that's why compare with the root
+            int sum = left.sum + right.sum + root.val;
+            globalMax = Math.max(sum, globalMax);
+
             return new NodeValue(
-                    Math.min(root.val, left.min),
                     Math.max(root.val, right.max),
-                    currentSum, 
-                    true);
+                    Math.min(root.val, left.min),
+                    sum);
         }
 
-        // If not a valid BST, mark isBST as false. 
-        // The sum here doesn't matter for the parent because it won't be used.
-        return new NodeValue(0, 0, 0, false);
+        //not valid bst then
+        return new NodeValue(Integer.MAX_VALUE, Integer.MIN_VALUE, 0);
+
     }
 
     public int maxSumBST(TreeNode root) {
-        maxSum = 0; // Reset global tracker
         largestBST(root);
-        return maxSum;
+        return globalMax;
     }
 }
