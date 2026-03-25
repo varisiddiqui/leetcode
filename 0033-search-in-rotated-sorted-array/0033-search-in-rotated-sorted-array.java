b73c752e0d9f4ce23a9bf430c68b1a7b0fb789dc
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-03-25 19:03:32 +0530

    Time: 0 ms (100%), Space: 43.6 MB (88.57%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,25 +1,26 @@
 class Solution {
     public int search(int[] nums, int target) {
         int n = nums.length;
-        int i=0;
-        while(i<n-1 && nums[i]<nums[i+1]) i++;
+        int low=0;
+        int high= n-1;
 
-        int ans1 = bin(0, i, nums, target);
-        int ans2 = bin(i+1, n-1, nums, target);
-
-        if(ans1 != -1) return ans1;
-        return ans2;
-    }
-
-    public int bin(int i, int j, int nums[], int target){
-        int low =i;
-        int high=j;
-
-        while(low <= high){
+        while(low<=high){
             int mid = low + (high-low)/2;
             if(nums[mid] == target) return mid;
-            else if(nums[mid] < target) low = mid+1;
-            else high = mid-1;
+            //check if left half is sorted
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && target <= nums[mid]){
+                    high = mid-1;
+                }
+                else low = mid+1;
+            }
+            //else right half is sorted
+            else {
+                if(nums[mid] <= target && target <= nums[high]){
+                    low = mid+1;
+                }
+                else high = mid-1;
+            }
         }
         return -1;
     }
