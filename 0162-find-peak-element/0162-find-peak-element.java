fb956b88b90d10042fe0ddb1e70eebdff3ecb605
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-03-27 11:22:33 +0530

    Time: 0 ms (100%), Space: 43.8 MB (97.97%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,44 +1,21 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         int n = nums.length;
-        int start = 0;
-        int end = n-1;
+        int low =1;
+        int high = n-2;
+
         if(n == 1) return 0;
-        if(n == 2){
-            if(nums[0]<nums[1]) return 1;
-            return 0;
-        }
-        int ans=0;
-        while(start<=end){
-            int mid = start + ((end-start)/2);
+        if(nums[0] > nums[1]) return 0;
+        if(nums[n-2] < nums[n-1]) return n-1;
+        int ans=-1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(nums[mid-1] < nums[mid] && nums[mid]> nums[mid+1]) return mid;
             
-            if((mid-1 >= 0 && mid+1<n) && (nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1])){
-                
-                ans = mid;
-                break;
-            }
-            else if((mid-1>=0 && mid+1>=n) && (nums[mid-1] < nums[mid])){
-                
-                    ans = mid;
-                    break;
-                
 
-            }
-
-            else if((mid-1<0 && mid+1<n) && (nums[mid] < nums[mid+1])){
-               
-                    ans = mid+1;
-                    break;
-                
-            }
-
-            else if (nums[mid] < nums[mid+1]){
-                start = mid+1;
-            }
-            else {
-                end = mid-1;
-            }
+            if(nums[mid] < nums[mid+1]) low = mid+1;
+            else high = mid-1;
         }
-        return ans;
+        return 0;
     }
 }
