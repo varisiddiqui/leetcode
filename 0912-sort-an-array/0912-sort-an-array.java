09ec620f1ee6c0e443ed00bdb33a8fb558ff3c3a
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-05-07 12:49:22 +0530

    Time: 40 ms (22.86%), Space: 86.3 MB (38.39%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,13 +1,47 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int num: nums) pq.add(num);
+        int n = nums.length;
 
-        int i=0;
-
-        while(!pq.isEmpty()){
-            nums[i++] = pq.poll();
+        // Build Max Heap
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(nums, n, i);
         }
+
+        // Extract elements one by one
+        for (int i = n - 1; i > 0; i--) {
+            swap(nums, 0, i);
+
+            // heap size reduces
+            heapify(nums, i, 0);
+        }
+
         return nums;
+    }
+
+    void heapify(int[] arr, int n, int i) {
+        int largest = i;
+
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            swap(arr, i, largest);
+
+            heapify(arr, n, largest);
+        }
+    }
+
+    void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
