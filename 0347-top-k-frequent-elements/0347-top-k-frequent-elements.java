d99e853a6a7f825b638163a4a58161e9631b6d27
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-05-07 12:38:31 +0530

    Time: 14 ms (71.06%), Space: 47.3 MB (90.47%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -2,29 +2,25 @@ class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         int n = nums.length;
         HashMap<Integer, Integer> hm = new HashMap<>();
-        for(int i=0; i<n; i++){
-            hm.put(nums[i], hm.getOrDefault(nums[i], 0) + 1);
-        }
+        
+        for(int num: nums) hm.put(num, hm.getOrDefault(num, 0)+1);
 
         Comparator<int[]> cmp = (a, b) -> {
-            return a[1] - b[1];
+            return Integer.compare(a[1], b[1]);
         };
 
         PriorityQueue<int[]> pq = new PriorityQueue<>(cmp);
-        for(Integer key: hm.keySet()){
+
+        for(int key: hm.keySet()){
             pq.add(new int[]{key, hm.get(key)});
-            if(pq.size()>k)
-            pq.remove();
+            if(pq.size()>k) pq.poll();
         }
 
         int ans[] = new int[k];
-        int l=0;
-        while(k-->0){
-           ans[l++]=pq.peek()[0];
-           pq.remove();
+        int i=0;
+        while(!pq.isEmpty()){
+            ans[i++] = pq.poll()[0];
         }
-
         return ans;
-        
     }
 }
