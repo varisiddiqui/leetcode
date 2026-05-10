4447c0bcc6782d0dd42c4c4f02ca531f967efcc7
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-05-10 12:51:52 +0530

    Time: 22 ms (4.56%), Space: 48.7 MB (15.71%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+
+        Comparator<int[]> cmp = (a, b) -> {
+            return Integer.compare(b[0], a[0]); 
+        };
+
+        PriorityQueue<int[]> rightGreater = new PriorityQueue<>(cmp);
+        PriorityQueue<Integer> leftGreater = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i=1; i<n; i++){
+            rightGreater.add(new int[]{height[i], i});
+        }
+
+        int water=0;
+
+        for(int i=0; i<n; i++){
+            int leftHt = (leftGreater.isEmpty())? height[i]: Math.max(leftGreater.peek(), height[i]);
+            leftGreater.add(height[i]);
+            
+            //right remove the all previous index
+
+            while(!rightGreater.isEmpty() && rightGreater.peek()[1]<=i) rightGreater.poll();
+
+            int rightHt = (rightGreater.isEmpty())? height[i]: Math.max(height[i], rightGreater.peek()[0]);
+
+            int waterHt = Math.min(leftHt, rightHt);
+
+            water += (waterHt - height[i]);
+        }
+        return water;
+    }
+}
