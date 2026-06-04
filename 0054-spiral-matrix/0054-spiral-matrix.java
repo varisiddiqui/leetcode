d67ca1009f9aa62c5bd01f45d5e355b25cb6f5bb
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-06-04 12:39:58 +0530

    Time: 0 ms (100%), Space: 42.9 MB (59.38%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -3,55 +3,59 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
 
-        boolean vis[][] = new boolean[m][n];
-        int i=0, j=0;
-
         List<Integer> li = new ArrayList<>();
-        li.add(matrix[0][0]);
-        vis[0][0]=true;
+
+        int left=0;
+        int right = n-1;
+
+        int top=0;
+        int bottom = m-1;
 
         while(true){
+         
+            for(int i=left; i<=right; i++){
+                li.add(matrix[top][i]);
+               // enter = true;
+            }
+            top++;
 
-            boolean enter=false;
+            if(top > bottom) break;
 
-            while(i<m && j+1<n && !vis[i][j+1]){ //top
-                li.add(matrix[i][j+1]);
-                vis[i][j+1]=true;
-                j++;
-                enter = true;
-                
+            for(int i=top; i<= bottom; i++){
+                li.add(matrix[i][right]);
+               // enter = true;
+            }
+            right--;
+
+            if(left > right) break;
+
+            for(int i=right; i>=left; i--){
+                li.add(matrix[bottom][i]);
+                //enter = true;
+            }
+            bottom--;
+            if(top > bottom) break;
+
+            for(int i=bottom; i>=top; i--){
+                li.add(matrix[i][left]);
+                //enter = true;
             }
 
-            while(i+1<m && j<n && !vis[i+1][j]){ //right
-                li.add(matrix[i+1][j]);
-                 vis[i+1][j]=true;
-                i++;
-                enter = true;
-               
-            }
+            left++;
+             if(left > right) break;
+        
+        }
 
-            while(i<m && j-1>=0 && !vis[i][j-1]){ //bottom
-                li.add(matrix[i][j-1]);
-                vis[i][j-1]=true;
-                j--;
-                 enter = true;
-                 
-            }
+        
 
-            while(i-1>=0 && j<n && !vis[i-1][j]){ //left
-                li.add(matrix[i-1][j]);
-                vis[i-1][j]=true;
-                i--;
-                 enter = true;
-                 
-            }
+        
 
-            //System.out.println(i+" "+j);
+        
 
-            if(!enter) break;
+           
 
             
-        }
+        
 
         return li;
     }
