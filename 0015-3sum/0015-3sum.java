823e2d5efd8d1e63220b39ce36a7bc9d9d638620
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-06-06 09:58:42 +0530

    Time: 719 ms (10.63%), Space: 59.9 MB (17.97%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,50 +1,43 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        int pos=0;
         int n = nums.length;
-
-       // while(pos <n && nums[pos]<0) pos++;
-
-        List<List<Integer>> li = new ArrayList<>();
-
-        if(pos == n ) return li;
+        Arrays.sort(nums);
 
         Set<List<Integer>> set = new HashSet<>();
 
-        while(pos < n){
-            int left = pos+1;
-            int right = n-1;
-            int req = -nums[pos];
-            int sum=0;
-            while(left < right){
     
-                if(left == pos) left++;
-                if(right == pos) right--;
-                if(left >= right) break;
-                sum = nums[left]+nums[right];
-
-                if(sum == req) {
-                    List<Integer> list = new ArrayList<>(Arrays.asList(nums[pos], nums[left], nums[right]));
-                    Collections.sort(list);
-                    set.add(list);
-                    left++;
-                    right--;
-                   
-                }
-                else if(sum < req){
-                    left++;
-                }
-                else right--;
-
-            }
-            pos++;
-
-        }
 
         
-        for(List<Integer> list : set) li.add(list);
+        for(int i=0; i<n; i++){
+            if(nums[i]>0) break;
+            int left=i+1; 
+            int right = n-1;
 
-        return li;
+            int target = -nums[i];
+
+            while(left<right){
+                if(nums[left]+nums[right] == target){
+                    List<Integer> li = new ArrayList<>(Arrays.asList(nums[i], nums[left], nums[right]));
+                    Collections.sort(li);
+                    set.add(li);
+                    left++;
+                    right--;
+
+                }
+                else if(nums[left]+nums[right] < target) left++;
+                else right--;
+            }
+
+            
+        }
+
+        return new ArrayList<>(set);
+
+        
+
+        
+
+       
+        
     }
 }
