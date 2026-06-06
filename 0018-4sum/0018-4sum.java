f1572631f065cb25224722510e57c452ff0e6992
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-06-06 17:33:59 +0530

    Time: 17 ms (89.94%), Space: 46.5 MB (13.99%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -6,8 +6,10 @@ class Solution {
         Set<List<Integer>> ans = new HashSet<>();
 
         for(int i=0; i<n; i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
 
             for(int j=i+1; j<n; j++){
+                if(j>i+1 && nums[j] == nums[j-1]) continue;
                 int left = j+1; 
                 int right = n-1;
 
