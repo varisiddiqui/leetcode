77f5ad39c9a1fada46846cb721f4921e0e2dbc10
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-06-06 17:27:16 +0530

    Time: 25 ms (12.36%), Space: 46.5 MB (13.88%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -20,6 +20,10 @@ class Solution {
 
                         left++;
                         right--;
+
+                       while(left<right && nums[left]==nums[left-1]) left++;
+
+                       while(left<right && nums[right]==nums[right+1]) right--;
                     }
                     else if(sum< tar){
                         left++;
