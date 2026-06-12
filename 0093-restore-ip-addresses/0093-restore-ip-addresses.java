784aa7396aeb1c153e021a5906b2ee56afda350e
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-06-12 10:18:48 +0530

    Time: 6 ms (37.86%), Space: 50.1 MB (22.15%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -1,78 +1,55 @@
 class Solution {
     public List<String> restoreIpAddresses(String s) {
-        int n = s.length();
-         List<String> ans = new ArrayList<>();
+        List<String> ans = new ArrayList<>();
 
-        if(n>12) return ans;
-
-        String part1 = "";
-        String part2 = "";
-        String part3 = "";
-        String part4 = "";
-
-        //int minL = Math.min(12, n);
-
-       
-
-        for(int i=0; i<3&& i<n; i++){
-            
-
-            part1 = s.substring(0, i+1);
-            //check validity
-            if(part1.length() > 1 && part1.charAt(0) == '0'){
-                continue;
-            }
-
-            int n1 = Integer.valueOf(part1);
-            if(n1<0 || n1 > 255){
-                continue;
-            }
-
-            for(int j=i+1; j<i+1+3 && j<n; j++){
-
-                part2 = s.substring(i+1, j+1);
-
-                if(part2.length()>1 && part2.charAt(0) == '0') continue;
-
-                int n2 = Integer.valueOf(part2);
-                if(n2<0 || n2 > 255){
-                    continue;
-                }   
-
-                for(int k=j+1; k<j+1+3 && k<n; k++){
-                    part3 = s.substring(j+1, k+1);
-
-                    if(part3.length() > 1 && part3.charAt(0) == '0') continue;
-
-                    int n3 = Integer.valueOf(part3);
-
-                    if(n3<0 || n3>255) continue;
-
-                    for(int l=k+1; l<k+1+3 && l<n; l++){
-                        part4 = s.substring(k+1, l+1);
-
-                        if(part4.length()>1 && part4.charAt(0) == '0') continue;
-
-                        int n4 = Integer.valueOf(part4);
-
-                        if(n4<0 || n4>255) continue;
-
-                        int len = part1.length()+part2.length()+part3.length()+part4.length();
-
-                         if(len != n) continue;
-
-                         ans.add(part1+"."+part2+"."+part3+"."+part4);
-
-                    }
-                }
-
-                
-            }
-
-            
-            
-        }
+        if(s.length() > 12) return ans;
+        findIp(s, "", 0, 0, s.length(), ans);
 
         return ans;
+    }
+
+    public void findIp(String s, String str, int stIdx, int parts, int n,
+    List<String> ans){
+        if(parts>4) return;
+
+        if(parts == 4 && stIdx==n){
+            ans.add(str.substring(0, str.length()-1));
+            return;
+        }
+
+        if(stIdx<n){
+            String part1 = s.substring(stIdx, stIdx+1); // this is the part
+            int num1 = Integer.valueOf(part1);
+
+            if((part1.length()>1 && part1.charAt(0) != '0' && (num1>=0 && num1<=255)) || (part1.length() == 1)){
+                
+                findIp(s, str+part1+".", stIdx+1, parts+1, n, ans);
+            }
+        }
+        
+
+        if(stIdx+1<n){
+            String part2 = s.substring(stIdx, stIdx+2); // this is the part
+            int num2 = Integer.valueOf(part2);
+
+            if((part2.length()>1 && part2.charAt(0) != '0'  && (num2>=0 && num2<=255)) ||  (part2.length() == 1)){
+                
+                findIp(s, str+part2+".", stIdx+2, parts+1, n, ans);
+            }
+        }
+        
+
+        if(stIdx+2<n){
+            String part3 = s.substring(stIdx, stIdx+3); // this is the part
+            int num3 = Integer.valueOf(part3);
+
+            if((part3.length()>1 && part3.charAt(0) != '0'  && (num3>=0 && num3<=255) ) || (part3.length() == 1)){
+                
+                findIp(s, str+part3+".", stIdx+3, parts+1, n, ans);
+            }
+        }
+        
+        
+
     }
 }
