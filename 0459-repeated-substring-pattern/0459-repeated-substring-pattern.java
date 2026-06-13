e47ea0d306a876676944697f44f85b150e612738
varisiddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-06-13 17:25:51 +0530

    Time: 74 ms (61.02%), Space: 46.9 MB (35.56%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,7 +1,5 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-       return (s + s).substring(1, s.length() * 2 - 1).contains(s);
-
-       
+        return (s+s).substring(1, s.length()*2-1).contains(s);
     }
 }
