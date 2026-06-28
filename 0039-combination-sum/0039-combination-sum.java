5b6ae462c9ce4678be273aaf07518849fa2198d7
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-06-28 12:54:06 +0530

    Time: 2 ms (95.87%), Space: 45.7 MB (67.21%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    Set<List<Integer>> ans = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
     
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         int n = candidates.length;
@@ -15,17 +15,17 @@ class Solution {
     public void subseq(int i, int[] candidates, List<Integer> li, int sum){
         if(i == candidates.length || sum<=0){
             if(sum == 0){
-                ans.add(li);
+                ans.add(new ArrayList<>(li));
             }
             return;
         }
 
 
         li.add(candidates[i]);
-        subseq(i+1, candidates, new ArrayList<>(li), sum-candidates[i]);
-        subseq(i, candidates, new ArrayList<>(li), sum-candidates[i]);
+        subseq(i, candidates, li, sum-candidates[i]);
 
         li.remove(li.size()-1);
-        subseq(i+1, candidates, new ArrayList<>(li), sum);
+       
+        subseq(i+1, candidates, li, sum);
     }
 }
