ee0908e634cbd3164f3034eebb40ddf4d48914ec
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-07-15 09:00:21 +0530

    Time: 2 ms (96.13%), Space: 43.9 MB (8.42%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -1,51 +1,30 @@
 class Solution {
     public int twoCitySchedCost(int[][] costs) {
-        int n = costs.length; //2n
+        int n = costs.length;
 
-        int temp[][] = new int[n][3];
-
+        int temp[][] = new int[n][2];
+        
         for(int i=0; i<n; i++){
-            temp[i][0] = i;//actual idx
-            temp[i][1] = Math.min(costs[i][0], costs[i][1]); //min of both cities
-            temp[i][2] = Math.abs(costs[i][0]-costs[i][1]); //diff of both to sort
+            temp[i][0] = i; //idx
+            temp[i][1] = costs[i][0]-costs[i][1]; //A-B
         }
 
         Comparator<int[]> cmp = (a, b) -> {
-            if(a[2] != b[2])
-            return Integer.compare(b[2], a[2]);
-            else return Integer.compare(a[1], b[1]);
+            return Integer.compare(b[1], a[1]);  //descending 
         };
 
         Arrays.sort(temp, cmp);
 
         int sum=0;
 
-        int a=0;
-        int b=0;
-
         for(int i=0; i<n; i++){
             int idx = temp[i][0];
-            
-            if(costs[idx][0] < costs[idx][1] && a<n/2){
-                sum += costs[idx][0];
-                a++;
-            }
-            else if(costs[idx][0] > costs[idx][1] && b<n/2){
-                sum += costs[idx][1];
-                b++;
-            }
-            else {
-                if(a<n/2) {
-                    sum += costs[idx][0];
-                    a++;
-                }
-                else{
-                    sum += costs[idx][1];
-                    b++;
-                }
-            }
-        }
 
+            if(i<n/2) sum += costs[idx][1];
+            else sum += costs[idx][0];
+        }
         return sum;
+
+
     }
 }
