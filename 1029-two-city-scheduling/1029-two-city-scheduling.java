0db566117c7c5afcd4fd6b7900c1705f8303433f
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-07-15 09:04:21 +0530

    Time: 2 ms (96.13%), Space: 43.4 MB (63.92%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -2,26 +2,21 @@ class Solution {
     public int twoCitySchedCost(int[][] costs) {
         int n = costs.length;
 
-        int temp[][] = new int[n][2];
         
-        for(int i=0; i<n; i++){
-            temp[i][0] = i; //idx
-            temp[i][1] = costs[i][0]-costs[i][1]; //A-B
-        }
 
         Comparator<int[]> cmp = (a, b) -> {
-            return Integer.compare(b[1], a[1]);  //descending 
+            return Integer.compare(b[0]-b[1], a[0]-a[1]);  //descending 
         };
 
-        Arrays.sort(temp, cmp);
+        Arrays.sort(costs, cmp);
 
         int sum=0;
 
         for(int i=0; i<n; i++){
-            int idx = temp[i][0];
+           
 
-            if(i<n/2) sum += costs[idx][1];
-            else sum += costs[idx][0];
+            if(i<n/2) sum += costs[i][1];
+            else sum += costs[i][0];
         }
         return sum;
 
