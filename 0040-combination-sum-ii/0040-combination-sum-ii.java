469a60808c7e07e89d0fa5ee569f9765a5af1f71
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-08-02 23:44:23 +0530

    Time: 7 ms (37.19%), Space: 45.4 MB (40.57%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,34 +1,29 @@
 class Solution {
     List<List<Integer>> ans = new ArrayList<>();
-
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        int n = candidates.length;
         Arrays.sort(candidates);
-
-        int i = 0;
-
-        subseq(i, candidates, target, new ArrayList<>());
+        subseq(candidates, 0, target, new ArrayList<>());
         return ans;
-
     }
 
-    public void subseq(int idx, int candidates[], int target, List<Integer> li){
-        if(idx == candidates.length || target <=0){
-            if(target == 0) ans.add(new ArrayList<>(li));
+    public void subseq(int nums[], int idx, int sum, List<Integer> li){
+
+        if(idx == nums.length || sum<=0) {
+            if(sum == 0) ans.add(new ArrayList<>(li));
             return;
         }
 
-        for(int i=idx; i<candidates.length; i++){ // for a particular index we consider
-            if(i>idx && candidates[i] == candidates[i-1]) continue;
-            if(candidates[i] > target) break;
+        for(int i=idx; i<nums.length; i++){
+            if(i>idx && nums[i] == nums[i-1]) continue; 
+           
+                li.add(nums[i]);
+                subseq(nums, i+1, sum-nums[i], li);
 
-
-                li.add(candidates[i]);
-                subseq(i+1, candidates, target-candidates[i], li);
-                li.remove(li.size()-1);    
+                li.remove(li.size()-1);
             
-
+            
         }
+
+
     }
-    
 }
