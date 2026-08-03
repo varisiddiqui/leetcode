ce5515a71c38bacdd76130263321a47922d9e2cc
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-08-03 14:50:41 +0530

    Time: 13 ms (15.9%), Space: 47.2 MB (12.32%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,29 +1,32 @@
 class Solution {
     List<List<Integer>> ans = new ArrayList<>();
+
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
         subseq(candidates, 0, target, new ArrayList<>());
         return ans;
     }
 
-    public void subseq(int nums[], int idx, int sum, List<Integer> li){
+    public void subseq(int nums[], int idx, int sum, List<Integer> li) {
 
-        if(idx == nums.length || sum<=0) {
-            if(sum == 0) ans.add(new ArrayList<>(li));
+        if (idx == nums.length || sum <= 0) {
+            if (sum == 0)
+                ans.add(new ArrayList<>(li));
             return;
         }
 
-        for(int i=idx; i<nums.length; i++){
-            if(i>idx && nums[i] == nums[i-1]) continue; 
-           
-                li.add(nums[i]);
-                subseq(nums, i+1, sum-nums[i], li);
+        HashSet<Integer> used = new HashSet<>();
 
-                li.remove(li.size()-1);
-            
-            
+        for (int i = idx; i < nums.length; i++) {
+
+            if (used.contains(nums[i]))
+                continue;
+            used.add(nums[i]);
+
+            li.add(nums[i]);
+            subseq(nums, i + 1, sum - nums[i], li);
+            li.remove(li.size() - 1);
         }
-
 
     }
 }
