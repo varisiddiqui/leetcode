055109fded689e4771ae6dbfa01f16d8f45d2fc0
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-08-13 16:52:14 +0530

    Time: 0 ms (100%), Space: 42.7 MB (57.37%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,21 +1,14 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if(n == 1) return nums[0];
-        if(n == 2) return Math.max(nums[0], nums[1]);
 
-        //in previous it was robbed till ith taking it.
-        // its max robbed till ith may or may not
-        int prev1 = Math.max(nums[0], nums[1]);
-        int prev2 = nums[0];
-        int curr =0;
+        int dp[] = new int[n+1];//max money robbed till ith house
 
-        for(int i=2; i<n; i++){
-         curr = Math.max(prev1, prev2+nums[i]);
-            prev2 = prev1;
-            prev1 = curr;
-        }
-        return curr; 
-        
+        dp[0]=0;
+        dp[1]=nums[0];
+
+        for(int i=2; i<n+1; i++) dp[i]= Math.max(nums[i-1]+dp[i-2], dp[i-1]);
+
+        return dp[n];
     }
 }
