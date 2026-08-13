1cc1f14eb3981eb91f277056c93e4a3800eaede7
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-08-13 18:37:31 +0530

    Time: 9 ms (11.67%), Space: 45.5 MB (62.53%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -2,27 +2,23 @@ class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
         int dp[][] = new int[n][n];
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                dp[i][j]=Integer.MAX_VALUE;
-            }
-        }
-        dp[0][0] = triangle.get(0).get(0);
-
+        dp[0][0]=triangle.get(0).get(0);
         for(int i=1; i<n; i++){
-            dp[i][0]= dp[i-1][0]+triangle.get(i).get(0);
-        }
+            for(int j=0; j<triangle.get(i).size(); j++){
+                int min = Integer.MAX_VALUE;
+                if(j-1>=0 && j-1<triangle.get(i-1).size())
+                 min = Math.min(min, dp[i-1][j-1]);
 
-        for(int i=1; i<n; i++){
-            for(int j=1; j<=i; j++){
-                dp[i][j] = Math.min(dp[i-1][j], dp[i-1][j-1]) + triangle.get(i).get(j);
+                if(j>=0 && j<triangle.get(i-1).size())
+                min = Math.min(min, dp[i-1][j]);
+                dp[i][j] = triangle.get(i).get(j)+min;
             }
         }
 
-        int min = Integer.MAX_VALUE;
-        for(int i=0; i<n; i++){
-            min = Math.min(min, dp[n-1][i]);
-        }
-        return min;
+        int min=Integer.MAX_VALUE;
+
+        for(int i=0; i<n; i++) min = Math.min(min, dp[n-1][i]);
+
+        return min; 
     }
 }
