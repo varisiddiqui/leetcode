6f2669e047b373600581e3ff054e17570f31a7f6
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-08-15 16:10:55 +0530

    Time: 48 ms (54.5%), Space: 47.5 MB (57.52%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,38 +1,27 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        //take total elements sum then try to half divide them 
         int n = nums.length;
         int sum=0;
-        for(int num: nums)
-        sum += num;
 
-        if(sum%2 != 0) return false;
+        for(int num: nums) sum += num;
 
-        int target = sum/2;
-        int dp[][] = new int[n+1][target+1];
+        if(sum % 2 != 0) return false;
 
-        //here max profit = max sum that can be obtained considering target as j
+        boolean dp[][] = new boolean[n+1][sum/2 + 1];
+
+        dp[0][0]=true;
+
+        for(int i=0; i<n+1; i++) dp[i][0] = true;
 
         for(int i=1; i<n+1; i++){
-            for(int j=1; j<target+1; j++){
-                //valid
+            for(int j=1; j<sum/2+1; j++){
                 if(nums[i-1] <= j){
-                    int include = dp[i-1][j-nums[i-1]] + nums[i-1];
-                    int exclude = dp[i-1][j];
-                    dp[i][j] = Math.max(include, exclude);
+                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
                 }
-                else{
-                    dp[i][j] = dp[i-1][j];
-                }
+                else dp[i][j] = dp[i-1][j];
             }
         }
 
-        if(dp[n][target] == target)
-        return true;
-        else
-        return false;
-
-
-
+        return dp[n][sum/2];
     }
 }
