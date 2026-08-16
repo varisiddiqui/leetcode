817af8360783b2d78aad8178d98cdec84ae2170d
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-08-16 17:22:42 +0530

    Time: 24 ms (33.42%), Space: 47.3 MB (14.11%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,28 +1,32 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
+       
+
         int dp[][] = new int[n+1][amount+1];
+         for(int i=0; i<n+1; i++) Arrays.fill(dp[i], Integer.MAX_VALUE);
 
-        //dp[i][j] = min no. coins in first i-coins that can make j
+       
 
-        for(int j=1; j<amount+1; j++){
-            dp[0][j] = Integer.MAX_VALUE-1;
-        }
+        for(int i=0; i<n+1; i++) dp[i][0] = 0;
+
+        
 
         for(int i=1; i<n+1; i++){
             for(int j=1; j<amount+1; j++){
                 //valid
-                if(coins[i-1]<=j){
-                    //include 
-                    int include = dp[i][j-coins[i-1]]+1;
-                    int exclude = dp[i-1][j];
-                    dp[i][j] = Math.min(include, exclude);
-                }
-                else
-                dp[i][j]=dp[i-1][j];
+               if(coins[i-1] <= j){
+                  if(dp[i][j-coins[i-1]] != Integer.MAX_VALUE) 
+                  dp[i][j] = dp[i][j-coins[i-1]]+1;
+                  dp[i][j] = Math.min(dp[i][j], dp[i-1][j]);  
+               }
+               else dp[i][j] = dp[i-1][j];
+                    
             }
         }
-        return dp[n][amount]>=Integer.MAX_VALUE-1? -1: dp[n][amount];
+
+        if(dp[n][amount] == Integer.MAX_VALUE) return -1;
+        return dp[n][amount];
 
     }
 }
