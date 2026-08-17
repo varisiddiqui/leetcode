b118e1cb913f18fef4d6b6bd59686c7a8c0d968f
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-08-17 12:21:54 +0530

    Time: 19 ms (85.6%), Space: 47 MB (68.9%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -2,40 +2,26 @@ class Solution {
     public int minInsertions(String s) {
         int n = s.length();
 
-        int[][] dp = new int[n][n];
+        int dp[][] = new int[n][n];
 
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
+        for(int i=0; i<n; i++) Arrays.fill(dp[i], -1);
+        return minIn(s, 0, n-1, dp);
 
-        return solve(s, 0, n - 1, dp);
+
     }
 
-    private int solve(String s, int left, int right, int[][] dp) {
+    public int minIn(String s, int left, int right, int dp[][]){
+        if(left >= right) return 0;
 
-        // Already a palindrome
-        if (left >= right) {
-            return 0;
+        if(dp[left][right] != -1) return dp[left][right];
+
+        if(s.charAt(left) == s.charAt(right)){
+            return dp[left][right]=minIn(s, left+1, right-1, dp);
         }
+        
+        return dp[left][right] = 1+Math.min(minIn(s, left+1, right,dp), minIn(s, left, right-1,dp)); 
+        
 
-        // Already calculated
-        if (dp[left][right] != -1) {
-            return dp[left][right];
-        }
-
-        // Characters match
-        if (s.charAt(left) == s.charAt(right)) {
-            dp[left][right] = solve(s, left + 1, right - 1, dp);
-        }
-
-        // Characters don't match
-        else {
-            dp[left][right] = 1 + Math.min(
-                solve(s, left + 1, right, dp),
-                solve(s, left, right - 1, dp)
-            );
-        }
-
-        return dp[left][right];
+        
     }
 }
