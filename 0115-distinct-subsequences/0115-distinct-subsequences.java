36a1100203032b3090e0899028fe647254d8dd13
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-08-20 15:32:43 +0530

    Time: 17 ms (81.01%), Space: 54.5 MB (29.94%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,23 +1,26 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        int n1=s.length();  
-        int n2=t.length();
+        int n1 = s.length();
+        int n2 = t.length();
 
         int dp[][] = new int[n1+1][n2+1];
-        dp[0][0]=1;
-        for(int i=1; i<n1+1; i++)
-        dp[i][0]=1;
-        for(int i=1; i<n1+1; i++){
-            for(int j=1; j<n2+1; j++){
-                if(s.charAt(i-1) == t.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
-                }
-                else{
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
 
-        return dp[n1][n2];
+        for(int i=0; i<n1; i++) Arrays.fill(dp[i], -1);
+
+        return subseq(s, t, s.length()-1, t.length()-1, dp);
+    }
+
+    public int subseq(String s, String t, int i, int j, int dp[][]){
+        if(j<0) return 1;
+        if(i<0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(s.charAt(i) == t.charAt(j)){
+            return dp[i][j]= (subseq(s, t, i-1, j, dp)+subseq(s, t, i-1, j-1, dp));
+        }
+        
+        return dp[i][j]=subseq(s, t, i-1, j, dp);
+
     }
 }
