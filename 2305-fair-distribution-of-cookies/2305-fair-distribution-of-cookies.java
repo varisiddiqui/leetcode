f5402fece3aab196a0217588a4328becf49674af
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-08-20 11:11:38 +0530

    Time: 82 ms (55.68%), Space: 42.7 MB (47.86%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
@@ -1,19 +1,19 @@
 class Solution {
     int ans=Integer.MAX_VALUE;
     public int distributeCookies(int[] cookies, int k) {
-        Map<Integer, Integer> hm = new HashMap<>();
-        for(int i=0; i<k; i++) hm.put(i, 0);
+        int hm[] = new int[8];
+        
 
         ans(cookies, cookies.length-1, k, hm);
 
         return ans;
     }
 
-    public void ans(int cookies[], int idx, int k, Map<Integer, Integer> hm){
+    public void ans(int cookies[], int idx, int k, int hm[]){
         if(idx < 0){
             int max=0;
-            for(int key: hm.keySet()){
-                max= Math.max(max, hm.get(key));
+            for(int key: hm){
+                max= Math.max(max, key);
             }
 
             ans = Math.min(ans, max);
@@ -23,10 +23,10 @@ class Solution {
     
 
         for(int i=0; i<k; i++){
-            if(ans < hm.get(i)+cookies[idx]) continue;
-            hm.put(i, hm.get(i)+cookies[idx]);
+            if(ans < hm[i]+cookies[idx]) continue;
+            hm[i] =hm[i]+cookies[idx];
             ans(cookies, idx-1, k, hm);
-            hm.put(i, hm.get(i)-cookies[idx]);
+            hm[i] = hm[i]-cookies[idx];
         }
     }
 }
