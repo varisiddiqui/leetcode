0523d49e456a2c21d0e3177fef8f255257cf722a
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-08-21 16:12:38 +0530

    Time: 140 ms (5%), Space: 182.5 MB (9.41%) - LeetHub

--- 4015-weighted-sum-of-a-tree/4015-weighted-sum-of-a-tree.java
+++ 4015-weighted-sum-of-a-tree/4015-weighted-sum-of-a-tree.java
@@ -1,28 +1,44 @@
 class Solution {
     public long weightedSum(int[] parent, int[] nums) {
         int n = parent.length;
+        if(n==1) return nums[0];
 
-        long sum = 0;
-        long[] dp = new long[n];
-        Arrays.fill(dp, -1);
+        HashMap<Integer, List<Integer>> parentOf = new HashMap<>();
 
-        dp[0] = 1;
-        long h = 1;
-
-        for (int i = 1; i < n; i++) {
-            h = Math.max(h, depth(i, parent, dp));
+        for(int i=1; i<n; i++){
+            int par = parent[i];
+            parentOf.putIfAbsent(par, new ArrayList<>());
+            parentOf.get(par).add(i);
         }
 
-        for (int i = 0; i < n; i++) {
-            sum += (long)nums[i] * (h - dp[i] + 1);
+        Queue<int[]> q = new LinkedList<>();
+
+        q.add(new int[]{0, 1}); //node, height
+        int depth[] = new int[n];
+
+        int h=1;
+    
+
+        while(!q.isEmpty()){
+            int top[] = q.remove();
+            depth[top[0]] = top[1];
+            h = Math.max(h, top[1]);
+
+            List<Integer> child = parentOf.getOrDefault(top[0], new ArrayList<>());
+        
+                for(int ch: child){
+                    q.add(new int[]{ch, top[1]+1});
+                }
+            
+
         }
 
+        long sum=0;
+
+        for(int i=0; i<n; i++){
+            sum += (long)nums[i] * 1L*(h-depth[i]+1);
+        }
         return sum;
-    }
-
-    public long depth(int node, int parent[], long dp[]){
-        if(dp[node] != -1) return dp[node];
-
-        return dp[node] = depth(parent[node], parent, dp)+1;
+        
     }
 }
