a28b5b2f729afff380e88f4ede35314a8795d12a
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-08-22 19:37:20 +0530

    Time: 53 ms (27.03%), Space: 47.8 MB (35.45%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -25,43 +25,5 @@ class Solution {
 
         return dp[n1][n2];
     }
-
-    public int isMat(String s, String p, int i, int j, int dp[][]){
-       
-        if(i==-1 && j==-1) return 1;
-        if(j<0) return 0;
-        if(i<0){
-            for(int k=0; k<=j; k++){
-                if(p.charAt(k) != '*') return 0;
-            }
-            return 1;
-        }
-        
-
-        
     
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-
-
-        if(s.charAt(i) == p.charAt(j)){
-            return dp[i][j]=isMat(s, p, i-1, j-1, dp);
-        }
-    
-            if(p.charAt(j) == '?'){
-                return dp[i][j]=isMat(s, p, i-1, j-1, dp);
-            }
-            else if(p.charAt(j) == '*'){
-                if(isMat(s, p, i, j-1, dp)==1) return dp[i][j]=1; 
-                if(isMat(s, p, i-1, j, dp)==1) return dp[i][j]=1;
-            }
-            return dp[i][j]=0;
-
-        
-
-            
-           
-        
-    }
 }
