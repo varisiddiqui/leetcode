d4a39bd177daa4bb1eeee8fbf02131b19e1bade5
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-08-22 10:03:02 +0530

    Time: 16 ms (72.14%), Space: 62.7 MB (21.98%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,38 +1,49 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        int m = s.length();
-        int n = p.length();
+        int n1 = s.length();
+        int n2 = p.length();
+        int dp[][] = new int[n1][n2];
+        for(int i=0; i<n1; i++) Arrays.fill(dp[i], -1);
 
-        boolean dp[][] = new boolean[m+1][n+1];
-        dp[0][0]=true;
-        for(int j=1; j<n+1; j++){
-            if(p.charAt(j-1) == '*'){
-                dp[0][j] = dp[0][j-1];
+        return isMat(s, p, n1-1, n2-1, dp)==1?true:false;
+    }
+
+    public int isMat(String s, String p, int i, int j, int dp[][]){
+       
+        if(i==-1 && j==-1) return 1;
+        if(j<0) return 0;
+        if(i<0){
+            for(int k=0; k<=j; k++){
+                if(p.charAt(k) != '*') return 0;
             }
-            else{
-                dp[0][j] = false;
+            return 1;
+        }
+        
+
+        
+    
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+
+
+        if(s.charAt(i) == p.charAt(j)){
+            return dp[i][j]=isMat(s, p, i-1, j-1, dp);
+        }
+    
+            if(p.charAt(j) == '?'){
+                return dp[i][j]=isMat(s, p, i-1, j-1, dp);
             }
-        }
-
-        for(int i=1; i<m+1; i++){
-            dp[i][0] = false;
-        }
-
-
-        for(int i=1; i<m+1; i++){
-            for(int j=1; j<n+1; j++){
-                if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?'){
-                    dp[i][j] = dp[i-1][j-1];
-                }
-                else if(p.charAt(j-1) == '*'){
-                    dp[i][j] = dp[i][j-1] || dp[i-1][j];
-                }
-                else{
-                    dp[i][j] = false;
-                }
+            else if(p.charAt(j) == '*'){
+                if(isMat(s, p, i, j-1, dp)==1) return dp[i][j]=1; 
+                if(isMat(s, p, i-1, j, dp)==1) return dp[i][j]=1;
             }
-        }
+            return dp[i][j]=0;
 
-        return dp[m][n];
+        
+
+            
+           
+        
     }
 }
