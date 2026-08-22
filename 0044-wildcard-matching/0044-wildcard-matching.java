17dcdae4d96fc7617d73cdb6454dfeef640c0797
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-08-22 10:10:28 +0530

    Time: 54 ms (24.51%), Space: 47.3 MB (63.62%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -2,10 +2,28 @@ class Solution {
     public boolean isMatch(String s, String p) {
         int n1 = s.length();
         int n2 = p.length();
-        int dp[][] = new int[n1][n2];
-        for(int i=0; i<n1; i++) Arrays.fill(dp[i], -1);
+        boolean dp[][] = new boolean[n1+1][n2+1];
 
-        return isMat(s, p, n1-1, n2-1, dp)==1?true:false;
+        dp[0][0] = true;
+
+        for(int j=1; j<n2+1; j++){
+            if(p.charAt(j-1) == '*') dp[0][j]=true;
+            else break;
+        }
+
+        for(int i=1; i<n1+1; i++){
+            for(int j=1; j<n2+1; j++){
+                if(s.charAt(i-1) == p.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    if(p.charAt(j-1) == '?') dp[i][j] = dp[i-1][j-1];
+                    if(p.charAt(j-1) == '*') dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+            }
+        }
+
+        return dp[n1][n2];
     }
 
     public int isMat(String s, String p, int i, int j, int dp[][]){
