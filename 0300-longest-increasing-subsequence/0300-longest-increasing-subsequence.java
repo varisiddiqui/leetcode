f99e08492645169ddf4b83c47200fde3d14d081b
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-08-23 19:22:18 +0530

    Time: 419 ms (5.25%), Space: 237.2 MB (14.27%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -2,19 +2,27 @@ class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
 
-        int dp[] = new int[n];
-        Arrays.fill(dp, 1);
-        int ans=1;
+        int dp[][] = new int[n][n+1];
 
-        for(int i=1; i<n; i++){
-            for(int j=i-1; j>=0; j--){
-                if(nums[j] < nums[i]){
-                    dp[i] = Math.max(dp[i], 1+dp[j]);
-                    ans = Math.max(ans, dp[i]);
-                }
-                
-            }
+        for(int i=0; i<n; i++)
+        Arrays.fill(dp[i], -1);
+        
+        return longest(0, -1, nums, dp);
+    }
+
+    public int longest(int currIdx, int prevIdx, int nums[], int dp[][]){
+        if(currIdx == nums.length) return 0;
+
+        if(dp[currIdx][prevIdx+1] != -1) return dp[currIdx][prevIdx+1];
+
+        int skip = longest(currIdx+1, prevIdx, nums, dp);
+
+        if(prevIdx == -1 || nums[prevIdx] < nums[currIdx]){
+            int take = 1 + longest(currIdx+1, currIdx, nums, dp);
+            return dp[currIdx][prevIdx+1] = Math.max(take, skip);
         }
-        return ans;
+
+        return dp[currIdx][prevIdx+1] = skip;
+
     }
 }
