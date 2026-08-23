176928238eba819e97e8f94df6c33a483f58ce9c
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-08-23 12:49:00 +0530

    Time: 129 ms (15.98%), Space: 134.1 MB (21.49%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,31 +1,42 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int dp[][][]=new int[n+1][2][3];     
+       
+        //dp[idx][buy] = maxprofit if buy or sell??
+        int dp[][][] = new int[n+1][2][3];
 
-        for(int i=n-1; i>=0; i--){
-            for(int buy=0; buy<=1; buy++){
-                for(int left=1; left<=2; left++){
-                    int profit=0;
-                    if(buy==1 && left>0){
-                        int take = -prices[i]+dp[i+1][0][left];
-                        int skip = 0+dp[i+1][1][left];
-                        profit = Math.max(take, skip);
-                    }
-                    else if(buy==0 && left>0){
-                        int take = prices[i]+dp[i+1][1][left-1];
-                        int skip = 0+dp[i+1][0][left];
-                        profit = Math.max(take, skip);
-                    }
-                    dp[i][buy][left] = profit;
-                }
+        for(int i=0; i<n+1; i++){
+            for(int j=0; j<2; j++){
+                for(int k=0; k<3; k++) dp[i][j][k] = -1;
             }
         }
-        return dp[0][1][2];
-
-
         
+
+        return maxPr(0, 1, prices, 0, dp);
     }
 
-   
+    public int maxPr(int idx, int buy, int prices[], int cnt, int dp[][][]){
+
+        if(idx == prices.length) return 0;
+        if(cnt == 2) return 0;
+
+        if(dp[idx][buy][cnt] != -1) return dp[idx][buy][cnt];
+
+        int  profit=0;
+        if(buy == 1){
+            int buyed = -prices[idx] + maxPr(idx+1, 0, prices, cnt, dp);
+            int notbuyed = 0 + maxPr(idx+1, 1, prices, cnt, dp);
+
+            profit = Math.max(profit, Math.max(buyed, notbuyed));
+        }
+
+        if(buy == 0){
+            int sold = prices[idx] + maxPr(idx+1, 1, prices, cnt+1, dp);
+            int notsold = 0 + maxPr(idx+1, 0, prices, cnt, dp);
+
+            profit = Math.max(sold, notsold);
+        }
+
+        return dp[idx][buy][cnt] = profit;
+    }
 }
