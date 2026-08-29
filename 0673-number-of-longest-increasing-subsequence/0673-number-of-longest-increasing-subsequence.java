72e4aa4f6f32f8561e28a888a3256d2a0e878718
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-08-29 16:57:40 +0530

    Time: 21 ms (47.72%), Space: 45.5 MB (43.05%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.java
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.java
@@ -4,39 +4,40 @@ class Solution {
         if(n == 1) return 1;
 
         int dp[]= new int[n];
+        int countWays[] = new int[n];
         Arrays.fill(dp, 1);
+        Arrays.fill(countWays, 1);
+
         int max=0;
-
-        for(int i=1; i<n; i++){
-            for(int j=i-1; j>=0; j--){
-                if(nums[j] < nums[i] && dp[i] < dp[j]+1){
-                    dp[i] = dp[j]+1;
-                }
-            }
-            max = Math.max(max, dp[i]);
-        }
-
-        int no[] = new int[n];
-        no[0]=1;
-
-        
-
-        for(int i=1; i<n; i++){
-            int req=dp[i]-1;
-            if(req == 0) {
-                no[i]++;
-                continue;
-            }
-            for(int j=i-1; j>=0; j--){
-                if(dp[j] == req && nums[j]<nums[i]) no[i] += no[j];
-            }
-        }
-
         int ans=0;
 
-        for(int i=0; i<n; i++){
-            if(dp[i] == max) ans += no[i];
+        for(int i=1; i<n; i++){
+            for(int j=i-1; j>=0; j--){
+                if(nums[j] < nums[i] && dp[i] <= dp[j]+1){
+                    if(dp[i] < dp[j]+1){
+                        dp[i] = dp[j]+1;
+                        countWays[i] = countWays[j];
+                    }
+                    else if(dp[i] == dp[j]+1){
+                        countWays[i] += countWays[j];
+                    }
+                }
+            }
+            if(max < dp[i]){
+                max = dp[i];
+                ans = countWays[i];
+            }
+            else if(max == dp[i]){
+                ans += countWays[i];
+            }
+            
         }
+        if(dp[0] == max) ans++;
+
+        
+        
+
+        
         return ans;
 
 
