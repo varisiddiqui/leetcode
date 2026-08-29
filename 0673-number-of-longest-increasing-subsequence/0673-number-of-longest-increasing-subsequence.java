ca5f44d0c644d3d3fe8cc6b510651511e4f3a008
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-08-29 16:36:16 +0530

    Time: 19 ms (76.46%), Space: 45.4 MB (59.78%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.java
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.java
@@ -1,41 +1,44 @@
 class Solution {
     public int findNumberOfLIS(int[] nums) {
         int n = nums.length;
-        if(n == 1){
-            return 1;
-        }
-        int dp[] = new int[n]; // lis
-        int count[] = new int[n]; //ways to make lis including it
+        if(n == 1) return 1;
+
+        int dp[]= new int[n];
         Arrays.fill(dp, 1);
-        Arrays.fill(count, 1);
-        
-        int max=1;
+        int max=0;
+
         for(int i=1; i<n; i++){
             for(int j=i-1; j>=0; j--){
-                if(nums[i]>nums[j] && dp[i]<=dp[j]+1){
-                    if(dp[i] < dp[j]+1){
-                        dp[i] = dp[j]+1;
-                        count[i]=count[j];
-                    }
-                    else{
-                        count[i] += count[j];
-                    }
-
+                if(nums[j] < nums[i] && dp[i] < dp[j]+1){
+                    dp[i] = dp[j]+1;
                 }
             }
             max = Math.max(max, dp[i]);
         }
-        if(max == 1)
-        return n;
-        int ways=0;
+
+        int no[] = new int[n];
+        no[0]=1;
+
+        
+
+        for(int i=1; i<n; i++){
+            int req=dp[i]-1;
+            if(req == 0) {
+                no[i]++;
+                continue;
+            }
+            for(int j=i-1; j>=0; j--){
+                if(dp[j] == req && nums[j]<nums[i]) no[i] += no[j];
+            }
+        }
+
+        int ans=0;
 
         for(int i=0; i<n; i++){
-            if(dp[i] == max)
-            ways += count[i];
-
+            if(dp[i] == max) ans += no[i];
         }
-        return ways;
-     
+        return ans;
+
 
     }
 }
