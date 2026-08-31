e4200bf96be0be52f161781de253b972dccede51
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-08-31 17:08:18 +0530

    Time: 42 ms (50.41%), Space: 47.4 MB (52.36%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,53 +1,50 @@
 class Solution {
-    int ans=Integer.MAX_VALUE;
+    int ans = Integer.MAX_VALUE;
+
     public int minCut(String s) {
         int n = s.length();
         int dp[] = new int[n];
-        Arrays.fill(dp,-1);
+        Arrays.fill(dp, -1);
 
         boolean isPal[][] = new boolean[n][n];
 
-        for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
-                if(isPal(i, j, s)) isPal[i][j] = true;
-                else isPal[i][j] = false;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+
+                if (s.charAt(i) == s.charAt(j) &&
+                        (j - i <= 1 || isPal[i + 1][j - 1])) {
+
+                    isPal[i][j] = true;
+                }
             }
         }
 
-        return part(0, s, dp, isPal)-1;
-        
-        
+        return part(0, s, dp, isPal) - 1;
+
     }
 
-    public int part(int idx, String s, int dp[], boolean isPal[][]){
-        if(idx == s.length()){
+    public int part(int idx, String s, int dp[], boolean isPal[][]) {
+        if (idx == s.length()) {
             return 0;
         }
 
-        if(dp[idx] != -1) return dp[idx];
+        if (dp[idx] != -1)
+            return dp[idx];
 
-        int cuts=s.length()+2;
-        
+        int cuts = s.length() + 2;
+
         //if(dp[idx] != -1) return dp[i];
 
-        for(int i=idx; i<s.length(); i++){
-            if(isPal[idx][i]){
-                
-               //System.out.println(li);
-                
-               cuts = Math.min(cuts, 1+part(i+1, s, dp, isPal));
-                
+        for (int i = idx; i < s.length(); i++) {
+            if (isPal[idx][i]) {
+
+                //System.out.println(li);
+
+                cuts = Math.min(cuts, 1 + part(i + 1, s, dp, isPal));
+
             }
         }
         return dp[idx] = cuts;
     }
 
-    public boolean isPal(int st, int end, String s){
-        while(st < end){
-            if(s.charAt(st) != s.charAt(end)) return false;
-            st++;
-            end--;
-        }
-        return true;
-    }
 }
