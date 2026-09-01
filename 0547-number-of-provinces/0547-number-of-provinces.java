1a577ea6f6270cc615fedffff42ad4e564540953
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-09-01 18:20:25 +0530

    Time: 6 ms (5.24%), Space: 46.9 MB (94.7%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -2,41 +2,34 @@ class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
 
-        
-        List<Integer> graph[] = new ArrayList[n];
-
-        Arrays.setAll(graph, (i) -> new ArrayList<>());
-
-        for(int i=0; i<n; i++){
-            int curr = i;
-            for(int j=0; j<n; j++){
-                int neigh = j;
-                if(isConnected[i][j] == 1){
-                    graph[curr].add(neigh);
-                }
-            }
-        }
+        boolean vis[][] = new boolean[n][n];
 
         int prv=0;
-        boolean vis[] = new boolean[n];
+
         for(int i=0; i<n; i++){
-            if(!vis[i]){
-                dfs(graph, vis, i);
-                prv++;
+            for(int j=0; j<n; j++){
+               
+                if(isConnected[i][j]==1 && !vis[i][j]){
+                    vis[i][j]=true;
+                    vis[j][i]=true;
+                    dfs(j, vis, isConnected);
+                    prv++;
+                }
             }
         }
         return prv;
 
-
     }
 
-    public void dfs(List<Integer> graph[], boolean vis[], int curr){
-        vis[curr] = true;
-
-        for(int neigh: graph[curr]){
-            if(!vis[neigh]){
-                dfs(graph, vis, neigh);
+    public void dfs(int i, boolean vis[][], int isConnected[][]){
+        for(int j=0; j<vis.length; j++){
+            if(isConnected[i][j]==1 && !vis[i][j]){
+                vis[i][j]=true;
+                vis[j][i]=true;
+                dfs(j, vis, isConnected);
             }
         }
     }
+
+
 }
