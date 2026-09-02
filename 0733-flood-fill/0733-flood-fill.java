8d3b3bd3a87367258b26a351ae7653fcf4cd872b
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-09-02 22:10:59 +0530

    Time: 1 ms (39.17%), Space: 47.1 MB (10.93%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,39 +1,44 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int r = image.length;
-        int c = image[0].length;
-        int org = image[sr][sc];
+        int m = image.length;
+        int n = image[0].length;
 
-        //boolean vis[][] = new boolean[r][c];
-    
-                dfs(org, image, sr, sc, color);
-        
-        
+        int original = image[sr][sc];
+
+        Queue<int[]> q = new LinkedList<>();
+
+        boolean vis[][] = new boolean[m][n];
+
+        q.add(new int[]{sr, sc});
+        vis[sr][sc] = true;
+
+        image[sr][sc]=color;
+
+        int d[][] = {{-1,0}, {0,-1}, {1,0}, {0, 1}};
+
+        while(!q.isEmpty()){
+            int sz = q.size();
+
+            for(int i=0; i<sz; i++){
+                int curr[] = q.remove();
+                int row = curr[0];
+                int col = curr[1];
+
+                for(int j=0; j<4; j++){
+                    int dr = d[j][0]+row;
+                    int dc = d[j][1]+col;
+                    if(dr >=0 && dr<m && dc>=0 && dc<n && !vis[dr][dc] && image[dr][dc]==original){
+                        vis[dr][dc]=true;
+                        image[dr][dc]=color;
+                        q.add(new int[]{dr, dc});
+                    }
+                }
+            }
+
+        }
+
         return image;
-    }
 
-    public void dfs(int org, int image[][], int i, int j, int color){
-       
-   
-        
-        int r = image.length;
-        int c = image[0].length;
 
-        image[i][j]=color;
-        if(i+1<r && image[i+1][j] == org&& image[i+1][j] != color){
-            dfs(org, image, i+1, j, color);
-        }
-
-        if(i-1>=0 && image[i-1][j] == org && image[i-1][j] != color){
-            dfs(org, image, i-1, j, color);
-        }
-
-        if(j+1<c && image[i][j+1] == org&& image[i][j+1] != color){
-            dfs(org, image, i, j+1, color);
-        }
-
-        if(j-1>=0 && image[i][j-1]==org&& image[i][j-1] != color){
-            dfs(org, image, i, j-1, color);
-        }
     }
 }
