f5f24434c987b76c8850dbaec41bd6b2d71ce87f
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-09-03 16:30:10 +0530

    Time: 17 ms (19.19%), Space: 49.1 MB (59.79%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,56 +1,47 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int r = mat.length;
-        int c = mat[0].length;
+        int m = mat.length;
+        int n = mat[0].length;
+        int ans[][] = new int[m][n];
 
-        int res[][] = new int[r][c];
-        boolean vis[][] = new boolean[r][c];
-        bfs(mat, res, vis);
-        return res;
-    }
-
-    public void bfs(int mat[][], int res[][], boolean vis[][]){
-        int r = mat.length;
-        int c = mat[0].length;
         Queue<int[]> q = new LinkedList<>();
 
-        for(int i=0; i<r; i++){
-            for(int j=0; j<c; j++){
-                if(mat[i][j] == 0){
-                    vis[i][j]=true;
-                    q.add(new int[]{i, j, 0});
+        boolean vis[][] = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0 && !vis[i][j]) {
+                    q.add(new int[] { i, j });
+                    vis[i][j] = true;
                 }
-                
             }
         }
 
+        int level = 0;
+        int d[][] = { { -1, 0 }, { 0, -1 }, { 0, 1 }, { 1, 0 } };
 
-        while(!q.isEmpty()){
-            int arr[] = q.remove();
-            int i=arr[0];
-            int j=arr[1];
-            int dis=arr[2];
-            res[i][j]=dis;
+        while (!q.isEmpty()) {
+            int sz = q.size();
+            level++;
 
-            if(i+1<r && mat[i+1][j] == 1 && !vis[i+1][j]){
-                vis[i+1][j]=true;
-                q.add(new int[]{i+1, j, dis+1});
-            }
-            if(i-1>=0 && mat[i-1][j] == 1 && !vis[i-1][j]){
-                vis[i-1][j]=true;
-                q.add(new int[]{i-1, j, dis+1});
-            }
-            if(j+1<c && mat[i][j+1] == 1 && !vis[i][j+1]){
-                vis[i][j+1]=true;
-                q.add(new int[]{i, j+1, dis+1});
-            }
-            if(j-1>=0 && mat[i][j-1] == 1 && !vis[i][j-1]){
-                vis[i][j-1]=true;
-                q.add(new int[]{i, j-1, dis+1});
+            for (int i = 0; i < sz; i++) {
+                int curr[] = q.remove();
+                for (int j = 0; j < 4; j++) {
+                    
+                    int row = d[j][0] + curr[0];
+                    int col = d[j][1] + curr[1];
+
+                    if (row >= 0 && row < m && col >= 0 && col < n && !vis[row][col]) {
+                        if (mat[row][col] == 1) {
+                            ans[row][col] = level;
+                        }
+                        vis[row][col] = true;
+                        q.add(new int[] { row, col });
+                    }
+                }
             }
         }
 
-
-
+        return ans;
     }
 }
