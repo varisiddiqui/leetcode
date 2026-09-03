da7ff888f386cfe07468fff517a621aac59f6700
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-09-03 20:38:50 +0530

    Time: 0 ms (100%), Space: 47.3 MB (37.76%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,33 +1,41 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        int n = graph.length;
-       
-        //boolean vis[] = new boolean[n];
-        int color[] = new int[n];
-        for(int i=0; i<n; i++){
-            if(color[i] == 0){
-                if(!dfs(i, -1, graph, color))
-                return false;
+        int v = graph.length;
+        boolean vis[] = new boolean[v];
+
+        int color[] = new int[v];
+        Arrays.fill(color, -1);
+
+        for(int i=0; i<v; i++){
+            if(!vis[i])            
+                if(!isValid(i, vis, graph, color, 0)) return false;                
+            }
+            
+        
+
+        return true;
+
+    }
+
+    public boolean isValid(int curr, boolean vis[], int graph[][], int color[], int clr){
+        vis[curr]=true;
+        color[curr]=clr;
+
+        for(int neigh: graph[curr]){
+            if(!vis[neigh]){
+                if(clr == 0){
+                    if(!isValid(neigh, vis, graph, color, 1)) return false;
+                }
+                else if(clr == 1){
+                    if(!isValid(neigh, vis, graph, color, 0)) return false;
+                }
+            }
+            else{
+                if(color[neigh] == clr) return false;
             }
         }
+
         return true;
-        
-  }
 
-  public boolean dfs(int curr, int par, int graph[][], int color[]){
-    if(par == -1){
-        color[curr] = 1;
     }
-    else if(color[par] == 1) color[curr]=2;
-    else color[curr]=1;
-
-    //vis[curr]=true;
-    for(int i=0; i<graph[curr].length; i++){
-        if(color[graph[curr][i]] == 0){
-            if(!dfs(graph[curr][i], curr, graph, color)) return false;
-        }
-        else if((color[graph[curr][i]] == 1 || color[graph[curr][i]] == 2) && graph[curr][i] != par &&  color[graph[curr][i]] == color[curr]) return false;
-    }
-    return true;
-  }
 }
