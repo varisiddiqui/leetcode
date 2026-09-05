0dfec38e14d8636f1a9d32d3cc91ff738d4fc2a0
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-09-05 15:56:16 +0530

    Time: 24 ms (44.48%), Space: 61 MB (70.7%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -7,53 +7,31 @@ class Solution {
 
         Arrays.setAll(graph, (i) -> new ArrayList<>());
 
+        int indegree[] = new int[v];
+        Queue<Integer> q = new LinkedList<>();
+
         for(int i=0; i<v; i++){
             for(int neigh: g[i]){
-                graph[i].add(neigh);
+                graph[neigh].add(i);
+                indegree[i]++;
+            }
+            if(indegree[i] == 0) q.add(i);
+        }
+
+        List<Integer> ans = new ArrayList<>();
+
+        while(!q.isEmpty()){
+            int curr = q.remove();
+            ans.add(curr);
+
+            for(int neigh: graph[curr]){
+                indegree[neigh]--;
+                if(indegree[neigh] == 0) q.add(neigh);
             }
         }
+        Collections.sort(ans);
+        return ans;
+
         
-        boolean vis[] = new boolean[v];
-        boolean pathVis[] = new boolean[v];
-        
-        
-
-        for(int i=0; i<v; i++){
-            if(!vis[i]){
-                isCycle(i, graph, vis, pathVis);
-            }
-        }
-
-        List<Integer> safe = new ArrayList<>();
-
-        for(int i=0; i<v; i++){
-            if(!pathVis[i]) safe.add(i);
-        }
-
-        return safe;
-
-    }
-
-    public boolean isCycle(int curr, List<Integer> graph[], boolean vis[], boolean pathVis[]){
-        vis[curr]=true;
-        pathVis[curr]=true;
-
-        for(int neigh: graph[curr]){
-            if(!vis[neigh]){
-                if(isCycle(neigh, graph, vis, pathVis)){
-                   
-                    return true;
-                }
-            }
-            else if(pathVis[neigh]){
-               
-                return true;
-            }
-            
-            
-        }
-        //isSafe[curr]=true;
-        pathVis[curr]=false;
-        return false;
     }
 }
