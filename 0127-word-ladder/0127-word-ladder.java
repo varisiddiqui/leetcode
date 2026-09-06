f3ab8f75b44387c22d1a773a4a8a4cac27f22ba0
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-09-06 17:19:23 +0530

    Time: 1593 ms (5.01%), Space: 47.4 MB (91.11%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,47 +1,79 @@
 class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        wordList.add(beginWord);
+        int n = wordList.size();
 
-     static class Pair {
-        String word;
-        int steps;
+        Map<String, List<String>> graph = new HashMap<>();
+        Map<String, Boolean> vis = new HashMap<>();
 
-        Pair(String word, int steps) {
-            this.word = word;
-            this.steps = steps;
+        
+
+        for(String str: wordList) {
+            graph.put(str, new ArrayList<>());
+            vis.put(str, false);
         }
-    }
 
-    public int ladderLength(String startWord, String targetWord, List<String> wordList) {
-          Queue<Pair> q = new LinkedList<>();
-            q.add(new Pair(startWord, 1));
-
-        Set<String> set = new HashSet<>(wordList);
-        set.remove(startWord);
-
-        while (!q.isEmpty()) {
-
-            Pair p = q.poll();
-            String word = p.word;
-            int steps = p.steps;
-
-            if (word.equals(targetWord)) return steps;
-
-            char[] arr = word.toCharArray();
-
-            for (int i = 0; i < arr.length; i++) {
-                char original = arr[i];
-
-                for (char ch = 'a'; ch <= 'z'; ch++) {
-                    arr[i] = ch;
-                    String next = new String(arr);
-
-                    if (set.contains(next)) {
-                        set.remove(next);
-                        q.add(new Pair(next, steps + 1));
-                    }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i == j) continue;
+                if(isValid(wordList.get(i), wordList.get(j))){
+                    graph.get(wordList.get(i)).add(wordList.get(j));
+                    graph.get(wordList.get(j)).add(wordList.get(i));
                 }
-                arr[i] = original; // restore
             }
         }
+
+       
+
+        int level=0;
+
+        Queue<String> q = new LinkedList<>();
+
+        vis.put(beginWord, true);
+        q.add(beginWord);
+
+        while(!q.isEmpty()){
+            int sz= q.size();
+            level++;
+
+            for(int i=0; i<sz; i++){
+                String curr = q.remove();
+
+                for(String neigh: graph.get(curr)){
+                    if(neigh.equals(endWord)) return level+1;
+                    if(!vis.get(neigh)){
+                        q.add(neigh);
+                        vis.put(neigh, true);
+                    }
+                }
+            }
+        }
+
         return 0;
+        
+
+
+    }
+
+    public boolean isValid(String s1, String s2){
+        int n1 = s1.length();
+        int n2 = s2.length();
+
+       
+        int lf=1;
+
+        int i=0;
+
+        int l = Math.min(n1, n2);
+
+        while(i<l){
+            if(s1.charAt(i) != s2.charAt(i) && lf == 0) break;
+            else if(s1.charAt(i) != s2.charAt(i) && lf==1) lf=0;
+
+            i++;
+        }
+       
+
+        return i==n1;
     }
 }
