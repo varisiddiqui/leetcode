aa68ceb7b69a39f31d6a97dce22d8a33fcda0a78
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-09-13 16:41:56 +0530

    Time: 58 ms (25.87%), Space: 47.8 MB (59.76%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,56 +1,51 @@
 class Solution {
     public int minimumEffortPath(int[][] heights) {
-        int r = heights.length;
-        int c = heights[0].length;
-        int diff[][] = new int[r][c];
-        for(int i=0; i<r; i++)
-        Arrays.fill(diff[i], Integer.MAX_VALUE);
-        diff[0][0] = 0;
+        int m = heights.length;
+        int n = heights[0].length;
+        
 
         Comparator<int[]> cmp = (a, b) -> {
-             return Integer.compare(a[0], b[0]);
+            return Integer.compare(a[2], b[2]);
         };
 
         PriorityQueue<int[]> pq = new PriorityQueue<>(cmp);
 
         pq.add(new int[]{0, 0, 0});
 
-        int dr[] = {0, 0, 1, -1};
-        int dc[] = {1, -1, 0, 0};
+        int d[][] = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+
+        int minEffort[][] = new int[m][n];
+
+        for(int i=0; i<m; i++) Arrays.fill(minEffort[i], Integer.MAX_VALUE);
+        minEffort[0][0]=0;
+      
+
+    
 
         while(!pq.isEmpty()){
             int top[] = pq.remove();
-            int i=top[1];
-            int j=top[2];
 
-            //means someone reached i,j at minimal diff than this so it will be in queue so we can't insert in this one because of greater efforts 
-            if(top[0] > diff[i][j]) continue;
+            int row = top[0];
+            int col = top[1];
+            int effort = top[2];
 
-            
+            for(int j=0; j<4; j++){
+                int dr = row+d[j][0];
+                int dc = col+d[j][1];
 
-            for(int k=0; k<4; k++){
-                int nr = i+dr[k];
-                int nc = j+dc[k];
-
-                if(nr>=0 && nr<r && nc>=0 && nc<c ){
-                    int difference = Math.max(top[0],Math.abs(heights[nr][nc]-heights[i][j]));
-                    if(diff[nr][nc] == Integer.MAX_VALUE){
-                        diff[nr][nc] = difference;
-                        pq.add(new int[]{difference, nr, nc});
-                    }
-                    else if(diff[nr][nc] > difference){
-                        diff[nr][nc] = difference;
-                        pq.add(new int[]{difference, nr, nc});
+                if(dr>=0 && dr<m && dc>=0 && dc<n){
+                    if(Math.max(effort, Math.abs(heights[row][col]-heights[dr][dc])) < minEffort[dr][dc]){
+                        minEffort[dr][dc] = Math.max(effort, Math.abs(heights[row][col]-heights[dr][dc]));
+                        pq.add(new int[]{dr, dc, minEffort[dr][dc]});
                     }
                 }
             }
+            
 
-            }
-            return diff[r-1][c-1];
+        }
 
+        return minEffort[m-1][n-1];
 
-
-        
 
 
     }
