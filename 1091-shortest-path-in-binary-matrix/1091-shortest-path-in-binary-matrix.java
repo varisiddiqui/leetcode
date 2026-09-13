9d4c3052811959a4e8be26c4497dc845cf6aae4d
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-09-13 15:51:53 +0530

    Time: 17 ms (41.4%), Space: 48.1 MB (13.02%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,51 +1,51 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-        if (grid[0][0] == 1)
-            return -1;
-        int r = grid.length;
-        int c = grid[0].length;
+        int m = grid.length;
+        int n = grid[0].length;
 
-        //boolean vis[][] = new boolean[r][c];
-        int dist[][] = new int[r][c];
-        for (int i = 0; i < r; i++)
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
+        if(grid[0][0] == 1 || grid[m-1][n-1]==1) return -1;
 
-        int dr[] = { 0, 0, -1, 1, -1, -1, 1, 1};
-        int dc[] = { 1, -1, 0, 0, -1 , 1, -1, 1};
+        boolean vis[][] = new boolean[m][n];
+
+        vis[0][0]=true;
 
         Queue<int[]> q = new LinkedList<>();
 
-        q.add(new int[] { 0, 0 });
-        //vis[source[0]][source[1]] = true;
-        dist[0][0] = 1;
+        q.add(new int[]{0,0});
 
-        while (!q.isEmpty()) {
-            int top[] = q.remove();
-            int i = top[0];
-            int j = top[1];
-            int d = dist[i][j];
-            for (int k = 0; k < dr.length; k++) {
-                int nr = i + dr[k];
-                int nc = j + dc[k];
+        int level=0;
 
-                if (nr >= 0 && nr < r && nc >= 0 && nc < c && grid[nr][nc] == 0 && dist[nr][nc] == Integer.MAX_VALUE) {
-                    q.add(new int[] { nr, nc });
-                    dist[nr][nc] = d + 1;
-                    if(nr== r-1 && nc == c-1) return d+1;
+        int d[][] = {{0,1}, {1,0}, {0,-1}, {-1,0}, {1,1}, {-1,-1}, {1,-1}, {-1,1}};
+       
+
+        while(!q.isEmpty()){
+            int sz = q.size();
+            level++;
+
+            for(int i=0; i<sz; i++){
+                
+                int top[] = q.remove();
+                int row = top[0];
+                int col = top[1];
+
+               // System.out.println(row+" "+col+" "+level);
+
+                if(row == m-1 && col == n-1) return level;
+
+                for(int j=0; j<8; j++){
+                    int dr = row+d[j][0];
+                    int dc = col+d[j][1];
+
+                    if(dr>=0 && dr<m && dc>=0 && dc<n && !vis[dr][dc] && grid[dr][dc]==0){
+                        q.add(new int[]{dr, dc});
+                        vis[dr][dc]=true;
+                    }
                 }
             }
-
         }
 
-        // for(int i=0; i<r; i++){
-        //     for(int j=0; j<c; j++){
-        //         System.out.print(dist[i][j]+" ");
-        //     }
-        //     System.out.println();
-        // }
-        if (dist[r - 1][c - 1] == Integer.MAX_VALUE)
-            return -1;
-        else
-            return dist[r - 1][c - 1];
+        return -1;
+
+
     }
 }
