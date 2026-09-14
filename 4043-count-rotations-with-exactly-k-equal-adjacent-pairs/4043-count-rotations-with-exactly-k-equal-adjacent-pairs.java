b4c9f10d9dea0da9ea489421c15ada47df3df8f9
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-09-14 20:42:48 +0530

    Time: 2 ms (66.58%), Space: 43.3 MB (99.33%) - LeetHub

--- 4043-count-rotations-with-exactly-k-equal-adjacent-pairs/4043-count-rotations-with-exactly-k-equal-adjacent-pairs.java
+++ 4043-count-rotations-with-exactly-k-equal-adjacent-pairs/4043-count-rotations-with-exactly-k-equal-adjacent-pairs.java
@@ -2,17 +2,28 @@ class Solution {
     public int countRotations(String s, int k) {
         int n = s.length();
         int ans=0;
+        int cnt=0;
+
+        for(int i=0; i<n-1; i++){
+            if(s.charAt(i) == s.charAt(i+1)) cnt++;
+        }
+
+        
+
+        StringBuilder str = new StringBuilder(s);
 
         for(int i=0; i<n; i++){
-            String st = s.substring(0,i+1);
-            String end = s.substring(i+1, n);
+            char first = str.charAt(0);
+            char last = str.charAt(n-1);
 
-            String con = end+st;
-            int cnt=0;
-            for(int j=0; j<con.length()-1; j++){
-                if(con.charAt(j) == con.charAt(j+1)) cnt++;
-            }
+            if(first == str.charAt(1)) cnt--;
+
+            if(first == last) cnt++;
+
             if(cnt == k) ans++;
+
+            str.deleteCharAt(0);
+            str.append(first);
         }
 
         return ans;
