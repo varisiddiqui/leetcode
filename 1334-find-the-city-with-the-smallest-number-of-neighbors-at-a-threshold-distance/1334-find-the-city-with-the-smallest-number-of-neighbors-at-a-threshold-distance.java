dd1062b1a9f3d9fd50728951a4d75f33c217fc4b
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-09-16 18:57:26 +0530

    Time: 9 ms (82.98%), Space: 46.3 MB (69.21%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,58 +1,44 @@
 class Solution {
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        int graph[][] = new int[n][n];
-    
+        int dist[][] = new int[n][n];
 
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++){
-                if(i != j)
-                graph[i][j] = Integer.MAX_VALUE;
-            }
+        for(int i=0; i<n; i++) Arrays.fill(dist[i], Integer.MAX_VALUE);
+
+        for(int i=0; i<n; i++) dist[i][i]=0;
+
+        for(int edg[]: edges){
+            dist[edg[0]][edg[1]] = edg[2];     
+            dist[edg[1]][edg[0]] = edg[2];
         }
         
-     
-
-        for(int i=0; i<edges.length; i++){
-            int src = edges[i][0];
-            int des = edges[i][1];
-            int wt = edges[i][2];
-
-            graph[src][des] = wt;
-            graph[des][src] = wt;
-        }
 
         for(int via=0; via<n; via++){
             for(int i=0; i<n; i++){
-                if(graph[i][via] == Integer.MAX_VALUE) continue;
                 for(int j=0; j<n; j++){
-                    if(graph[via][j] == Integer.MAX_VALUE) continue;
-                    graph[i][j] = Math.min(graph[i][j], (graph[i][via]+graph[via][j]));
+                    if(dist[i][via] == Integer.MAX_VALUE || dist[via][j] == Integer.MAX_VALUE) continue;
+                        dist[i][j] = Math.min(dist[i][j], dist[i][via]+dist[via][j]);
+                    
                 }
             }
         }
+
+        int smallestCity=-1;
+
+        int minReachable=n+1;
 
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++) 
-            System.out.println(graph[i][j]+" ");
-            System.out.println();
+            int currReach=0;
+            for(int j=0; j<n; j++){
+                if(dist[i][j] <= distanceThreshold) currReach++;
+            }
+            if(currReach < minReachable){
+                minReachable = currReach;
+                smallestCity = i;
+            }
+            else if(currReach == minReachable) smallestCity = Math.max(smallestCity, i);
         }
 
-        int max=n+1;
-        int city=-1;
-        for(int i=0; i<n; i++){//src
-            int reachable=0;
-            for(int j=0; j<n; j++){//des
-                if(graph[i][j] <= distanceThreshold){
-                    reachable++;
-                }
-            }
-            if(max >= reachable){
-                max = reachable;
-                city = Math.max(city, i);
-            }
-        }
-        return city;
+        return smallestCity;
 
-        
     }
 }
