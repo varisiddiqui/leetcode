bc4c3236b0e8f36166bf94349e9c6bfcd410aac9
Mohammad Varis Siddiqui <166267134+varisiddiqui@users.noreply.github.com>
2026-09-17 19:36:20 +0530

    Time: 7 ms (53.74%), Space: 69.1 MB (27.62%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,57 +1,53 @@
 class Solution {
-    
     public int makeConnected(int n, int[][] connections) {
+        Set<Integer> set = new HashSet<>();
+
+        int rank[] = new int[n];
         int par[] = new int[n];
-        int sz[] = new int[n];
 
-        for(int i=0; i<n; i++){
-            par[i]=i;
-            sz[i]=1;
-        }
-        int extra=0;
+        for(int i=0; i<n; i++) par[i]=i;
 
-        for(int i=0; i<connections.length; i++){
-            int x = connections[i][0];
-            int y = connections[i][1];
+        int extra = 0;
 
-            if(find(par, x) == find(par, y)){
+        for(int edg[]: connections){
+            int x = edg[0];
+            int y = edg[1];
+            
+            if(find(x, par) == find(y, par)){ // means they are already connected
                 extra++;
                 continue;
             }
-            else{
-                union(par, sz, x, y);
+            else {
+                union(x, y, par, rank);
             }
         }
 
-        int cnt=0;
-        for(int i=0; i<n; i++){
-            if(find(par, i) == i) cnt++;
-        }
+        for(int i=0; i<n; i++) set.add(find(i, par));
 
-        return (extra<(cnt-1))?-1: cnt-1;
+        return (extra >= set.size()-1)?set.size()-1: -1;
+
+    
     }
 
-    static int find(int par[], int x){
+    public void union(int x, int y, int par[], int rank[]){
+        int px = find(x, par);
+        int py = find(y, par);
+
+        if(rank[px] == rank[py]){
+            par[px] = py;
+            rank[py]++;
+        }
+        else if(rank[px] < rank[py]){
+            par[px] = py;
+        }
+        else {
+            par[py] = px;
+        } 
+    }
+
+    public int find(int x, int par[]){
         if(x == par[x]) return x;
-        else return par[x] = find(par, par[x]);
-    }
 
-    static void union(int par[], int sz[], int x, int y){
-        int parX = find(par, x);
-        int parY = find(par, y);
-
-        if(parX == parY) {
-           
-            return;
-        }
-
-        if(sz[parX] < sz[parY]){
-            par[parX] = parY;
-            sz[parY] += sz[parX];
-        }
-        else{
-            par[parY] = parX;
-            sz[parX] += sz[parY];
-        }
+        return par[x] = find(par[x], par);
     }
 }
